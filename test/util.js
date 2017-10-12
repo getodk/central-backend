7e54c6bc0197b4852b3d45f9ab4c03a2056c668c
Matthew White <matthew-white@users.noreply.github.com>
2017-10-12 17:14:16 -0400

    Remove some tests

--- test/util.js
+++ test/util.js
@@ -11,21 +11,6 @@ describe('util', () => {
       merge({ a: 1, c: 3 }, { b: 2, c: 4 }).should.eql({ a: 1, b: 2, c: 4 });
     });
 
-    it('should not concatenate arrays', () => {
-      merge({ a: 1, c: [ 3, 4 ] }, { b: 2, c: [ 5, 6 ] })
-        .should.eql({ a: 1, b: 2, c: [ 5, 6 ] });
-    });
-
-    it('should merge null values', () => {
-      merge({ a: 1, c: 3 }, { b: 2, c: null })
-        .should.eql({ a: 1, b: 2, c: null });
-    });
-
-    it('should merge undefined values', () => {
-      merge({ a: 1, c: 3 }, { b: 2, c: undefined })
-        .should.eql({ a: 1, b: 2, c: undefined });
-    });
-
     it('should not mutate its arguments', () => {
       const first = { a: 1 };
       merge(first, { b: 2 });
