fd0117884066acf2f206a0bbab750bcb3bc3c5a4
Matthew White <matthew-white@users.noreply.github.com>
2017-10-12 00:35:25 -0400

    Update merge()
    Closes #11.

--- test/util.js
+++ test/util.js
@@ -6,15 +6,30 @@ describe('util', () => {
     it('should merge two plain shallow objects', () => {
       merge({ a: 1 }, { b: 2 }).should.eql({ a: 1, b: 2 });
     });
-    it('should favor later attributes over earlier ones', () => {
-      merge({ a: 1, c: 3 }, { b: 2, c: 5 }).should.eql({ a: 1, b: 2, c: 5 });
+
+    it('should favor later properties over earlier ones', () => {
+      merge({ a: 1, c: 3 }, { b: 2, c: 4 }).should.eql({ a: 1, b: 2, c: 4 });
     });
-    it('should merge arrays if it finds them', () => {
-      merge({ a: 1, c: [ 3, 4 ] }, { b: 2, c: [ 5, 6 ] }).should.eql({ a: 1, b: 2, c: [ 3, 4, 5, 6 ] });
+
+    it('should not concatenate arrays', () => {
+      merge({ a: 1, c: [ 3, 4 ] }, { b: 2, c: [ 5, 6 ] })
+        .should.eql({ a: 1, b: 2, c: [ 5, 6 ] });
     });
-    it('should ignore null parameters', () => {
-      merge({ a: 1, c: 3 }, { b: 2, c: null }).should.eql({ a: 1, b: 2, c: 3 });
+
+    it('should merge null values', () => {
+      merge({ a: 1, c: 3 }, { b: 2, c: null })
+        .should.eql({ a: 1, b: 2, c: null });
+    });
+
+    it('should merge undefined values', () => {
+      merge({ a: 1, c: 3 }, { b: 2, c: undefined })
+        .should.eql({ a: 1, b: 2, c: undefined });
+    });
+
+    it('should not mutate its arguments', () => {
+      const first = { a: 1 };
+      merge(first, { b: 2 });
+      first.should.eql({ a: 1 });
     });
   });
 });
-
