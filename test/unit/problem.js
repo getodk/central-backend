a254e09781d64a35ae9b33cee4163a76d60813c7
Clint Tseng <clint@dontexplain.com>
2017-12-26 16:24:36 -0800

    test: some dumb silly tests for Problem.

---
+++ test/unit/problem.js
@@ -0,0 +1,14 @@
+const should = require('should');
+const Problem = require('../../lib/problem');
+
+// we aren't going to test the many problem types here, only the basic infrastructure.
+describe('Problem', () => {
+  it('should flag as a Problem', () => {
+    (new Problem()).isProblem.should.equal(true);
+  });
+
+  it('should truncate the detailed code to give the http code', () => {
+    (new Problem(404.42)).httpCode.should.equal(404);
+  });
+});
+
