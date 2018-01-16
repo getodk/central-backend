75789cae654ae1a2ae2c0bf6287af10305d861bb
Clint Tseng <clint@dontexplain.com>
2018-01-16 15:32:01 -0800

    test: minor changes to api test harness based on code review.
    * warn if a mail transport other than JSON is used.
    * sort the fixture file list to ensure consistency.
    * make the more-than-one-fixture case actually work.

--- test/api/setup.js
+++ test/api/setup.js
@@ -18,7 +18,10 @@ const owner = config.get('test.database.user');
 
 // set up our mailer.
 const { mailer } = require(appRoot + '/lib/outbound/mail');
-const mail = mailer(config.get('test.email'));
+const mailConfig = config.get('test.email');
+const mail = mailer(mailConfig);
+if (mailConfig.transport !== 'json')
+  console.error('WARNING: some tests will not work except with a JSON email transport configuration.');
 
 // application things.
 const injector = require(appRoot + '/lib/model/package');
@@ -28,9 +31,10 @@ const service = require(appRoot + '/lib/service');
 const fixtures = readdirSync(appRoot + '/test/api/fixtures')
   .filter((name) => /^\d\d-[a-z-_]+\.js$/i.test(name))
   .map((name) => join(appRoot.toString(), '/test/api/fixtures', /^([^.]+)\.js$/i.exec(name)[1]))
+  .sort()
   .map(require);
 const populate = (container, [ head, ...tail ] = fixtures) =>
-  (tail.length === 0) ? head(container).point() : head(container).then(() => populate(container, tail));
+  (tail.length === 0) ? head(container) : head(container).then(() => populate(container, tail));
 
 // set up the database at the very beginning of the suite; wipe the database,
 // run the standard migrations, then run the fixture scripts to populate our
@@ -42,7 +46,7 @@ before(() => db
   .raw('drop owned by ' + owner)
   //.raw('drop owned by ?', [ owner ]) TODO: why does this <- not work?
   .then(() => db.migrate.latest({ directory: appRoot + '/lib/model/migrations' }))
-  .then(() => populate(injector.withDefaults({ db }))));
+  .then(() => populate(injector.withDefaults({ db })).point()));
 
 // augments a supertest object with a `.as(user, cb)` method, where user may be the
 // name of a fixture user or an object with email/password. the user will be logged
