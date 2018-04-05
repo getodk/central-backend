1056836bd425924dd6d7e7353d2f220b72f61361
Clint Tseng <clint@dontexplain.com>
2018-04-05 16:37:56 -0700

    test: tests for nested odata repeat output.

--- test/unit/data/json.js
+++ test/unit/data/json.js
@@ -130,5 +130,105 @@ describe('submissionToOData', () => {
       }]);
     });
   });
+
+  it('should provide navigation links for repeats', () => {
+    const fields = schemaAsLookup(getFormSchema({ xml: testData.forms.withrepeat }));
+    const submission = { instanceId: 'two', xml: testData.instances.withrepeat.two };
+    return submissionToOData(fields, 'Submissions', submission).then((result) => {
+      result.should.eql([{
+        __id: 'two',
+        meta: { instanceID: 'two' },
+        name: 'Bob',
+        age: 34,
+        children: {
+          'child@odata.navigationLink': "Submissions('two')/children/child"
+        }
+      }]);
+    });
+  });
+
+  it('should extract subtable rows within repeats', () => {
+    const fields = schemaAsLookup(getFormSchema({ xml: testData.forms.withrepeat }));
+    const submission = { instanceId: 'two', xml: testData.instances.withrepeat.two };
+    return submissionToOData(fields, 'Submissions.children.child', submission).then((result) => {
+      result.should.eql([{
+        '__Submissions-id': 'two',
+        __id: 'cf9a1b5cc83c6d6270c1eb98860d294eac5d526d',
+        age: 4,
+        name: 'Billy'
+      }, {
+        '__Submissions-id': 'two',
+        __id: 'c76d0ccc6d5da236be7b93b985a80413d2e3e172',
+        age: 6,
+        name: 'Blaine'
+      }]);
+    });
+  });
+
+  it('should return navigation links to repeats within a subtable result set', () => {
+    const fields = schemaAsLookup(getFormSchema({ xml: testData.forms.doubleRepeat }));
+    const submission = { instanceId: 'double', xml: testData.instances.doubleRepeat.double };
+    return submissionToOData(fields, 'Submissions.children.child', submission).then((result) => {
+      result.should.eql([{
+        __id: '46ebf42ee83ddec5028c42b2c054402d1e700208',
+        '__Submissions-id': 'double',
+        name: 'Alice'
+      }, {
+        __id: 'b6e93a81a53eed0566e65e472d4a4b9ae383ee6d',
+        '__Submissions-id': 'double',
+        name: 'Bob',
+        toys: {
+          'toy@odata.navigationLink': "Submissions('double')/children/child('b6e93a81a53eed0566e65e472d4a4b9ae383ee6d')/toys/toy"
+        }
+      }, {
+        __id: '8954b393f82c1833abb19be08a3d6cb382171f54',
+        '__Submissions-id': 'double',
+        name: 'Chelsea',
+        toys: {
+          'toy@odata.navigationLink': "Submissions('double')/children/child('8954b393f82c1833abb19be08a3d6cb382171f54')/toys/toy"
+        }
+      }]);
+    });
+  });
+
+  it('should return second-order subtable results', () => {
+    const fields = schemaAsLookup(getFormSchema({ xml: testData.forms.doubleRepeat }));
+    const submission = { instanceId: 'double', xml: testData.instances.doubleRepeat.double };
+    return submissionToOData(fields, 'Submissions.children.child.toys.toy', submission).then((result) => {
+      result.should.eql([{
+        __id: 'a9058d7b2ed9557205ae53f5b1dc4224043eca2a',
+        '__Submissions-children-child-id': 'b6e93a81a53eed0566e65e472d4a4b9ae383ee6d',
+        name: 'Twilight Sparkle'
+      }, {
+        __id: '8d2dc7bd3e97a690c0813e646658e51038eb4144',
+        '__Submissions-children-child-id': 'b6e93a81a53eed0566e65e472d4a4b9ae383ee6d',
+        name: 'Pinkie Pie'
+      }, {
+        __id: 'b716dd8b79a4c9369d6b1e7a9c9d55ac18da1319',
+        '__Submissions-children-child-id': 'b6e93a81a53eed0566e65e472d4a4b9ae383ee6d',
+        name: 'Applejack'
+      }, {
+        __id: '52fbd613acc151ee1187026890f6246b35f69144',
+        '__Submissions-children-child-id': 'b6e93a81a53eed0566e65e472d4a4b9ae383ee6d',
+        name: 'Spike'
+      }, {
+        __id: '4a4a05249c8f992b0b3cc7dbe690b57cf18e8ea9',
+        '__Submissions-children-child-id': '8954b393f82c1833abb19be08a3d6cb382171f54',
+        name: 'Rainbow Dash'
+      }, {
+        __id: '00ae97cddc4804157e3a0b13ff9e30d86cfd1547',
+        '__Submissions-children-child-id': '8954b393f82c1833abb19be08a3d6cb382171f54',
+        name: 'Rarity'
+      }, {
+        __id: 'ecc1d831ae487ceef09ab7ccc0a021d3cab48988',
+        '__Submissions-children-child-id': '8954b393f82c1833abb19be08a3d6cb382171f54',
+        name: 'Fluttershy'
+      }, {
+        __id: 'd6539297765d97b951c6a63d7f70cafeb1741f8d',
+        '__Submissions-children-child-id': '8954b393f82c1833abb19be08a3d6cb382171f54',
+        name: 'Princess Luna'
+      }]);
+    });
+  });
 });
 
