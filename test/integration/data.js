445be7e83b81c0470b110b5850d67d77ae16a748
Clint Tseng <clint@dontexplain.com>
2018-04-05 16:37:56 -0700

    test: tests for nested odata repeat output.

--- test/integration/data.js
+++ test/integration/data.js
@@ -110,6 +110,57 @@ module.exports = {
       <label>What is your age?</label>
     </input>
   </h:body>
+</h:html>`,
+
+    doubleRepeat: `<?xml version="1.0"?>
+<h:html xmlns="http://www.w3.org/2002/xforms" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:ev="http://www.w3.org/2001/xml-events" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms">
+  <h:head>
+    <model>
+      <instance>
+        <data id="doubleRepeat" orx:version="1.0">
+          <orx:meta>
+            <orx:instanceID/>
+          </orx:meta>
+          <name/>
+          <children>
+            <child>
+              <name/>
+              <toys>
+                <toy>
+                  <name/>
+                </toy>
+              </toys>
+            </child>
+          </children>
+        </data>
+      </instance>
+      <bind nodeset="/data/meta/instanceID" preload="uid" type="string"/>
+      <bind nodeset="/data/name" type="string"/>
+      <bind nodeset="/data/children/child/name" type="string"/>
+      <bind nodeset="/data/children/child/toys/toy/name" type="string"/>
+    </model>
+  </h:head>
+  <h:body>
+    <input ref="/data/name">
+      <label>What is your name?</label>
+    </input>
+    <group ref="/data/children/child">
+      <label>Child</label>
+      <repeat nodeset="/data/children/child">
+        <input ref="/data/children/child/name">
+          <label>What is the child's name?</label>
+        </input>
+        <group ref="/data/children/child/toys">
+          <label>Child</label>
+          <repeat nodeset="/data/children/child/toys/toy">
+            <input ref="/data/children/child/toys/toy/name">
+              <label>What is the toy's name?</label>
+            </input>
+          </repeat>
+        </group>
+      </repeat>
+    </group>
+  </h:body>
 </h:html>`
   },
   instances: {
@@ -127,6 +178,35 @@ module.exports = {
       one: instance('simple2', 'one', '<name>Alice</name><age>30</age>'),
       two: instance('simple2', 'two', '<name>Bob</name><age>34</age>'),
       three: instance('simple2', 'three', '<name>Chelsea</name><age>38</age>')
+    },
+    doubleRepeat: {
+      double: `<data>
+    <meta><instanceID>double</instanceID></meta>
+    <name>Vick</name>
+    <children>
+      <child>
+        <name>Alice</name>
+      </child>
+      <child>
+        <name>Bob</name>
+        <toys>
+          <toy><name>Twilight Sparkle</name></toy>
+          <toy><name>Pinkie Pie</name></toy>
+          <toy><name>Applejack</name></toy>
+          <toy><name>Spike</name></toy>
+        </toys>
+      </child>
+      <child>
+        <name>Chelsea</name>
+        <toys>
+          <toy><name>Rainbow Dash</name></toy>
+          <toy><name>Rarity</name></toy>
+          <toy><name>Fluttershy</name></toy>
+          <toy><name>Princess Luna</name></toy>
+        </toys>
+      </child>
+    </children>
+  </data>`
     }
   }
 };
