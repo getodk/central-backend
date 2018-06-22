637d8e4955e9fe21e64b07f147fb1f774fe8af02
Clint Tseng <clint@dontexplain.com>
2018-06-22 16:28:03 -0700

    test #42: import briefcase's own csv export tests for verification.
    * i actually had to modify them extensively, because we:
      * don't touch DateTimes or Times at all, we just passthrough.
      * don't output SET-OF-x.
      * don't reformat SubmissionDate to Java MEDIUM.
    * but otherwise at least we have double verification of join-id
      generation.

--- test/unit/data/briefcase.js
+++ test/unit/data/briefcase.js
@@ -25,7 +25,7 @@ const mockForm = (data) => {
   return data;
 };
 
-describe('.csv.zip briefcase output', () => {
+describe('.csv.zip briefcase output @slow', () => {
   it('should output a simple flat table within a zip', (done) => {
     const form = mockForm({
       xmlFormId: 'mytestform',
@@ -323,6 +323,238 @@ Porg plushie,two/children/child[2],two/children/child[2]/toy[2]
 Millennium Falcon,three/children/child[1],three/children/child[1]/toy[1]
 X-Wing,three/children/child[1],three/children/child[1]/toy[2]
 Pod racer,three/children/child[1],three/children/child[1]/toy[3]
+`);
+      done();
+    });
+  });
+
+  it('briefcase replicated test: all-data-types', (done) => {
+    const form = mockForm({
+      xmlFormId: 'all-data-types',
+      xml: `
+<?xml version="1.0"?>
+<h:html xmlns="http://www.w3.org/2002/xforms" xmlns:h="http://www.w3.org/1999/xhtml">
+  <h:head>
+    <h:title>All data types</h:title>
+    <model>
+      <instance>
+        <data id="all-data-types">
+          <some_string/>
+          <some_int/>
+          <some_decimal/>
+          <some_date/>
+          <some_time/>
+          <some_date_time/>
+          <some_geopoint/>
+          <some_geotrace/>
+          <some_geoshape/>
+          <some_barcode/>
+          <meta>
+            <instanceID/>
+          </meta>
+        </data>
+      </instance>
+      <bind nodeset="/data/some_string" type="string"/>
+      <bind nodeset="/data/some_int" type="int"/>
+      <bind nodeset="/data/some_decimal" type="decimal"/>
+      <bind nodeset="/data/some_date" type="date"/>
+      <bind nodeset="/data/some_time" type="time"/>
+      <bind nodeset="/data/some_date_time" type="dateTime"/>
+      <bind nodeset="/data/some_geopoint" type="geopoint"/>
+      <bind nodeset="/data/some_geotrace" type="geotrace"/>
+      <bind nodeset="/data/some_geoshape" type="geoshape"/>
+      <bind nodeset="/data/some_barcode" type="barcode"/>
+      <bind calculate="concat('uuid:', uuid())" nodeset="/data/meta/instanceID" readonly="true()" type="string"/>
+    </model>
+  </h:head>
+  <h:body>
+    <input ref="some_string"/>
+    <input ref="some_int"/>
+    <input ref="some_decimal"/>
+    <input ref="some_date"/>
+    <input ref="some_time"/>
+    <input ref="some_date_time"/>
+    <input ref="some_geopoint"/>
+    <input ref="some_geotrace"/>
+    <input ref="some_geoshape"/>
+    <input ref="some_barcode"/>
+  </h:body>
+</h:html>`
+    });
+    const inStream = streamTest.fromObjects([{
+      instanceId: 'uuid:39f3dd36-161e-45cb-a1a4-395831d253a7',
+      createdAt: '2018-04-26T08:58:20.525Z',
+      xml: `
+<data id="all-data-types" instanceID="uuid:39f3dd36-161e-45cb-a1a4-395831d253a7" submissionDate="2018-04-26T08:58:20.525Z" isComplete="true" markedAsCompleteDate="2018-04-26T08:58:20.525Z" xmlns="http://opendatakit.org/submissions">
+  <some_string>Hola</some_string>
+  <some_int>123</some_int>
+  <some_decimal>123.456</some_decimal>
+  <some_date>2018-04-26</some_date>
+  <some_time>08:56:00.000Z</some_time>
+  <some_date_time>2018-04-26T08:56:00.000Z</some_date_time>
+  <some_geopoint>43.3149254 -1.9869671 71.80000305175781 15.478</some_geopoint>
+  <some_geotrace>43.314926 -1.9869713 71.80000305175781 10.0;43.3149258 -1.9869694 71.80000305175781 10.0;43.3149258 -1.9869694 71.80000305175781 10.0;</some_geotrace>
+  <some_geoshape>43.31513313655808 -1.9863833114504814 0.0 0.0;43.31552832470026 -1.987161487340927 0.0 0.0;43.315044828733015 -1.9877894595265388 0.0 0.0;43.31459255404834 -1.9869402050971987 0.0 0.0;43.31513313655808 -1.9863833114504814 0.0 0.0;</some_geoshape>
+  <some_barcode>000049499094</some_barcode>
+  <n0:meta xmlns:n0="http://openrosa.org/xforms">
+    <n0:instanceID>uuid:39f3dd36-161e-45cb-a1a4-395831d253a7</n0:instanceID>
+  </n0:meta>
+</data>`
+    }]);
+
+    callAndParse(form, inStream, (result) => {
+      result.filenames.should.containDeep([ 'all-data-types.csv' ]);
+      result['all-data-types.csv'].should.equal(
+`SubmissionDate,some_string,some_int,some_decimal,some_date,some_time,some_date_time,some_geopoint-Latitude,some_geopoint-Longitude,some_geopoint-Altitude,some_geopoint-Accuracy,some_geotrace,some_geoshape,some_barcode,meta-instanceID,KEY
+2018-04-26T08:58:20.525Z,Hola,123,123.456,2018-04-26,08:56:00.000Z,2018-04-26T08:56:00.000Z,43.3149254,-1.9869671,71.80000305175781,15.478,43.314926 -1.9869713 71.80000305175781 10.0;43.3149258 -1.9869694 71.80000305175781 10.0;43.3149258 -1.9869694 71.80000305175781 10.0;,43.31513313655808 -1.9863833114504814 0.0 0.0;43.31552832470026 -1.987161487340927 0.0 0.0;43.315044828733015 -1.9877894595265388 0.0 0.0;43.31459255404834 -1.9869402050971987 0.0 0.0;43.31513313655808 -1.9863833114504814 0.0 0.0;,000049499094,uuid:39f3dd36-161e-45cb-a1a4-395831d253a7,uuid:39f3dd36-161e-45cb-a1a4-395831d253a7
+`);
+      done();
+    });
+  });
+
+  it('briefcase replicated test: nested-repeats', (done) => {
+    const form = mockForm({
+      xmlFormId: 'nested-repeats',
+      xml: `
+<h:html xmlns="http://www.w3.org/2002/xforms" xmlns:h="http://www.w3.org/1999/xhtml" xmlns:jr="http://openrosa.org/javarosa">
+  <h:head>
+    <h:title>Nested repeats</h:title>
+    <model>
+      <instance>
+        <data id="nested-repeats">
+          <meta>
+            <instanceID/>
+          </meta>
+          <g1 jr:template="">
+            <t1/>
+            <g2 jr:template="">
+              <t2/>
+              <g3 jr:template="">
+                <t3/>
+              </g3>
+            </g2>
+          </g1>
+        </data>
+      </instance>
+      <itext>
+        <translation lang="English">
+          <text id="/data/g1:label">
+            <value></value>
+          </text>
+          <text id="/data/g1/g2:label">
+            <value></value>
+          </text>
+          <text id="/data/g1/g2/g3:label">
+            <value></value>
+          </text>
+        </translation>
+      </itext>
+      <bind nodeset="/data/meta/instanceID" type="string" readonly="true()" calculate="concat('uuid:', uuid())"/>
+      <bind nodeset="/data/g1/t1" type="string"/>
+      <bind nodeset="/data/g1/g2/t2" type="string"/>
+      <bind nodeset="/data/g1/g2/g3/t3" type="string"/>
+    </model>
+  </h:head>
+  <h:body>
+    <group>
+      <label ref="jr:itext('/data/g1:label')"/>
+      <repeat nodeset="/data/g1">
+        <input ref="/data/g1/t1">
+        </input>
+        <group>
+          <label ref="jr:itext('/data/g1/g2:label')"/>
+          <repeat nodeset="/data/g1/g2">
+            <input ref="/data/g1/g2/t2">
+            </input>
+            <group>
+              <label ref="jr:itext('/data/g1/g2/g3:label')"/>
+              <repeat nodeset="/data/g1/g2/g3">
+                <input ref="/data/g1/g2/g3/t3">
+                </input>
+              </repeat>
+            </group>
+          </repeat>
+        </group>
+      </repeat>
+    </group>
+  </h:body>
+</h:html>`
+    });
+    const inStream = streamTest.fromObjects([{
+      instanceId: 'uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b',
+      createdAt: '2018-02-01T11:35:19.178Z',
+      xml: `
+<data id="nested-repeats" instanceID="uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b" version="2018012404" submissionDate="2018-02-01T11:35:19.178Z" isComplete="true" markedAsCompleteDate="2018-02-01T11:35:19.178Z" xmlns="http://opendatakit.org/submissions">
+  <g1>
+    <t1>some text 1</t1>
+    <g2>
+      <t2>some text 1.1</t2>
+      <g3>
+        <t3>some text 1.1.1</t3>
+      </g3>
+    </g2>
+    <g2>
+      <t2>some text 1.2</t2>
+    </g2>
+  </g1>
+
+  <g1>
+    <t1>some text 2</t1>
+    <g2>
+      <t2>some text 2.1</t2>
+    </g2>
+  </g1>
+
+  <g1>
+    <t1>some text 3</t1>
+    <g2>
+      <t2>some text 3.1</t2>
+      <g3>
+        <t3>some text 3.1.1</t3>
+      </g3>
+      <g3>
+        <t3>some text 3.1.2</t3>
+      </g3>
+      <g3>
+        <t3>some text 3.1.3</t3>
+      </g3>
+      <g3>
+        <t3>some text 3.1.4</t3>
+      </g3>
+    </g2>
+  </g1>
+  <n0:meta xmlns:n0="http://openrosa.org/xforms">
+    <n0:instanceID>uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b</n0:instanceID>
+  </n0:meta>
+</data>`
+    }]);
+
+    callAndParse(form, inStream, (result) => {
+      result.filenames.should.containDeep([ 'nested-repeats.csv', 'nested-repeats-g1.csv', 'nested-repeats-g1-g2.csv', 'nested-repeats-g1-g2-g3.csv' ]);
+      result['nested-repeats.csv'].should.equal(
+`SubmissionDate,meta-instanceID,KEY
+2018-02-01T11:35:19.178Z,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b
+`);
+      result['nested-repeats-g1.csv'].should.equal(
+`t1,PARENT_KEY,KEY
+some text 1,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[1]
+some text 2,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[2]
+some text 3,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]
+`);
+      result['nested-repeats-g1-g2.csv'].should.equal(
+`t2,PARENT_KEY,KEY
+some text 1.1,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[1],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[1]/g2[1]
+some text 1.2,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[1],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[1]/g2[2]
+some text 2.1,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[2],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[2]/g2[1]
+some text 3.1,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1]
+`);
+      result['nested-repeats-g1-g2-g3.csv'].should.equal(
+`t3,PARENT_KEY,KEY
+some text 1.1.1,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[1]/g2[1],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[1]/g2[1]/g3[1]
+some text 3.1.1,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1]/g3[1]
+some text 3.1.2,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1]/g3[2]
+some text 3.1.3,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1]/g3[3]
+some text 3.1.4,uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1],uuid:0a1b861f-a5fd-4f49-846a-78dcf06cfc1b/g1[3]/g2[1]/g3[4]
 `);
       done();
     });
