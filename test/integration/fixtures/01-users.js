fcb186d265f241a8577d31830cbbde047ccaaad1
Clint Tseng <clint@dontexplain.com>
2018-11-07 16:56:31 -0800

    improve: some minor code cleanup from code review: comments, nits.

--- test/integration/fixtures/01-users.js
+++ test/integration/fixtures/01-users.js
@@ -8,7 +8,7 @@ module.exports = ({ all, User, Actor, Membership }) => {
 
   // hash the passwords, create our three test users, then add Alice to administrators.
   // mark transaction so the whole thing happens in one commit rather than many (slow).
-  return all.mapSequential(users, (user) => user.withHashedPassword(user.password))
+  return Promise.all(users.map((user) => user.withHashedPassword(user.password)))
     .then((users) => all.mapSequential(users, (user) => user.create()))
     .then(([ alice, bob, chelsea ]) => alice.actor.addToSystemGroup('admins'));
 };
