52e1ec94b068ea6eeeb84514fd748b73721c77b7
Issa Tseng <maintainer@heyissa.com>
2019-07-06 20:02:01 -0700

    test: test mixed clear/enc submission decrypt. fix some misc syntax.

--- test/integration/other/encryption.js
+++ test/integration/other/encryption.js
@@ -151,7 +151,7 @@ describe('managed encryption', () => {
     it('should decrypt to CSV successfully', testService((service) =>
       service.login('alice', (asAlice) =>
         asAlice.post('/v1/projects/1/key')
-          .send({ passphrase: 'supersecret', hint: 'it is a secret', })
+          .send({ passphrase: 'supersecret', hint: 'it is a secret' })
           .expect(200)
           .then(() => asAlice.get('/v1/projects/1/forms/simple.xml')
             .expect(200)
@@ -183,7 +183,7 @@ describe('managed encryption', () => {
     it('should decrypt attached files successfully', testService((service) =>
       service.login('alice', (asAlice) =>
         asAlice.post('/v1/projects/1/key')
-          .send({ passphrase: 'supersecret', hint: 'it is a secret', })
+          .send({ passphrase: 'supersecret', hint: 'it is a secret' })
           .expect(200)
           .then(() => asAlice.get('/v1/projects/1/forms/simple.xml')
             .expect(200)
@@ -203,6 +203,41 @@ describe('managed encryption', () => {
               result['media/charlie'].should.equal('file charlie is right here');
               done();
             }))))));
+
+    it('should handle mixed[encrypted/plaintext] source records', testService((service) =>
+      service.login('alice', (asAlice) =>
+        asAlice.post('/v1/projects/1/forms/simple/submissions')
+          .send(testData.instances.simple.one)
+          .set('Content-Type', 'text/xml')
+          .expect(200)
+          .then(() => asAlice.post('/v1/projects/1/key')
+            .send({ passphrase: 'supersecret', hint: 'it is a secret' })
+            .expect(200)
+            .then(() => asAlice.get('/v1/projects/1/forms/simple.xml')
+              .expect(200)
+              .then(({ text }) => sendEncrypted(asAlice, extractVersion(text), extractPubkey(text)))
+              .then((send) => send(testData.instances.simple.two)
+                .then(() => send(testData.instances.simple.three))))
+            .then(() => asAlice.get('/v1/projects/1/forms/simple/submissions/keys')
+              .expect(200)
+              .then(({ body }) => body[0].id))
+            .then((keyId) => new Promise((done) =>
+              zipStreamToFiles(asAlice.get(`/v1/projects/1/forms/simple/submissions.csv.zip?${keyId}=supersecret`), (result) => {
+                result.filenames.should.eql([ 'simple.csv' ]);
+
+                // TODO?: copied from the equivalent test in integration/api/submissions:
+                const csv = result['simple.csv'].split('\n').map((row) => row.split(','));
+                csv.length.should.equal(5); // header + 3 data rows + newline
+                csv[0].should.eql([ 'SubmissionDate', 'meta-instanceID', 'name', 'age', 'KEY', 'SubmitterID', 'SubmitterName' ]);
+                csv[1].shift().should.be.an.recentIsoDate();
+                csv[1].should.eql([ 'three','Chelsea','38','three', '5', 'Alice' ]);
+                csv[2].shift().should.be.an.recentIsoDate();
+                csv[2].should.eql([ 'two','Bob','34','two', '5', 'Alice' ]);
+                csv[3].shift().should.be.an.recentIsoDate();
+                csv[3].should.eql([ 'one','Alice','30','one', '5', 'Alice' ]);
+                csv[4].should.eql([ '' ]);
+                done();
+              })))))));
   });
 });
 
