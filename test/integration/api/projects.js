cf6756b08bcfbe90295f5b4cc2d7214815623ec4
Issa Tseng <maintainer@heyissa.com>
2019-07-29 14:58:03 -0700

    bug: need to include keyId in Extended Project response as well.

--- test/integration/api/projects.js
+++ test/integration/api/projects.js
@@ -409,9 +409,15 @@ describe('api: /projects', () => {
         asAlice.post('/v1/projects/1/key')
           .send({ passphrase: 'supersecret', hint: 'it is a secret' })
           .expect(200)
-          .then(() => asAlice.get('/v1/projects/1')
-            .expect(200)
-            .then(({ body }) => { body.keyId.should.be.a.Number(); })))));
+          .then(() => Promise.all([
+            asAlice.get('/v1/projects/1')
+              .expect(200)
+              .then(({ body }) => { body.keyId.should.be.a.Number(); }),
+            asAlice.get('/v1/projects/1')
+              .set('X-Extended-Metadata', true)
+              .expect(200)
+              .then(({ body }) => { body.keyId.should.be.a.Number(); })
+          ])))));
 
     it('should modify extant forms', testService((service) =>
       service.login('alice', (asAlice) =>
