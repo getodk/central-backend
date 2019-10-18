d535621323a73bdd2ae1d4cdd907d09678ac7243
Issa Tseng <maintainer@heyissa.com>
2019-10-18 15:45:21 -0700

    new: implement PartialPipe for improved piped stream error handling.
    * nodejs streams have long had this problem:
      const result = x.pipe(y).pipe(z);
      result.on('error', () => { /* will never fire for errors on x/y */ });
    * for a long time, the pump package was the way to solve this, but as of
      node 10.x the stream package includes pipeline() which behaves
      similarly.
    * but pump and pipeline work a bit differently:
      pipeline(x, y, z, (err) => { /* called on completion */ });
      where err is populated only if an error occurred.
    * this is fine for a lot of cases, but it assumes that the point of pipe
      assembly is the point that makes the most sense for error handling.
    * this doesn't play well with our architecture, wherein we frequently
      have functions which return a Stream as their retval and assume the
      stream's error handling will handle errors. many of these functions
      assemble streams into pipes as a part of solving their computation
      problem, and handling the error there doesn't work.
    * so we implement PartialPipe, which really just remembers a set of
      streams to be assembled into a pipe eventually, and at the (presently
      two) locations where piped streams are consumed, we teach them to deal
      with PartialPipes explicitly.

--- test/unit/util/stream.js
+++ test/unit/util/stream.js
@@ -1,6 +1,7 @@
 require('should');
 const appPath = require('app-root-path');
-const { mapStreamToPromises, splitStream } = require(appPath + '/lib/util/stream');
+const { Transform } = require('stream');
+const { mapStreamToPromises, splitStream, PartialPipe } = require(appPath + '/lib/util/stream');
 const Option = require(appPath + '/lib/util/option');
 const { fromObjects, toObjects } = require('streamtest').v2;
 
@@ -49,6 +50,75 @@ describe('stream utils', () => {
         x.should.eql([ 4, 8, 15, 16, 23, 42 ]);
         y.should.eql([ 4, 8, 15, 16, 23, 42 ]);
       }));
+  });
+
+  describe('PartialPipe', () => {
+    const noop = () => {};
+    const doubler = () => new Transform({
+      objectMode: true,
+      transform(x, _, done) { done(null, x * 2); }
+    });
+
+    it('should pipeline constructor-given streams when requested', (done) =>
+      (new PartialPipe([
+        fromObjects([ 4, 8, 15, 16, 23, 42 ]), doubler(), doubler(), toObjects((e, result) => {
+          result.should.eql([ 16, 32, 60, 64, 92, 168 ]);
+          done();
+        })
+      ])).pipeline(noop));
+
+    it('should pipeline of-given streams when requested', (done) =>
+      PartialPipe.of(
+        fromObjects([ 4, 8, 15, 16, 23, 42 ]), doubler(), doubler(), toObjects((e, result) => {
+          result.should.eql([ 16, 32, 60, 64, 92, 168 ]);
+          done();
+        })
+      ).pipeline(noop));
+
+    it('should append streams with with', (done) =>
+      PartialPipe.of(fromObjects([ 4, 8, 15, 16, 23, 42 ]), doubler())
+        .with(doubler())
+        .with(toObjects((e, result) => {
+            result.should.eql([ 16, 32, 60, 64, 92, 168 ]);
+            done();
+          })
+        ).pipeline(noop));
+
+    it('should not callback if there is no error', (done) => {
+      let cb = false;
+      PartialPipe.of(
+        fromObjects([ 4, 8, 15, 16, 23, 42 ]), doubler(), doubler(), toObjects(noop)
+      ).pipeline(() => {
+        cb = true;
+      }).on('finish', () => {
+        setTimeout(() => {
+          cb.should.equal(false);
+          done();
+        }, 0);
+      });
+    });
+
+    it('should callback if there is an error', (done) => {
+      const raiser = () => new Transform({
+        objectMode: true,
+        transform(x, _, done) { (x > 20) ? done(new Error('whoops')) : done(null, x * 2); }
+      });
+
+      PartialPipe.of(
+        fromObjects([ 4, 8, 15, 16, 23, 42 ]), raiser(), doubler(), toObjects(noop)
+      ).pipeline((err) => {
+        err.message.should.equal('whoops');
+        done();
+      });
+    });
+
+    it('should assemble a traditional .pipe() chain on pipe', (done) =>
+      PartialPipe.of(
+        fromObjects([ 4, 8, 15, 16, 23, 42 ]), doubler(), doubler()
+      ).pipe(toObjects((e, result) => {
+        result.should.eql([ 16, 32, 60, 64, 92, 168 ]);
+        done();
+      })));
   });
 });
 
--- test/unit/util/zip.js
+++ test/unit/util/zip.js
@@ -1,8 +1,11 @@
 const appRoot = require('app-root-path');
 const should = require('should');
+const { Transform } = require('stream');
 const { zipStreamToFiles } = require(appRoot + '/test/util/zip');
 const { streamAttachments } = require(appRoot + '/lib/data/attachments');
+const { PartialPipe } = require(appRoot + '/lib/util/stream');
 const { zipPart, zipStreamFromParts } = require(appRoot + '/lib/util/zip');
+const { fromChunks } = require('streamtest').v2;
 
 describe('zipPart streamer', () => {
   it('should close the archive only after parts are finalized', (done) => {
@@ -52,6 +55,80 @@ describe('zipPart streamer', () => {
     part1.finalize();
 
     part2.append('test 4', { name: 'y/test4.file' });
+    part2.finalize();
+  });
+
+  it('should process PartialPipe inputs appropriately', (done) => {
+    const part1 = zipPart();
+    const part2 = zipPart();
+
+    zipStreamToFiles(zipStreamFromParts(part1, part2), (result) => {
+      result.filenames.should.containDeep([ 'test1.file', 'test2.file' ]);
+      result['test1.file'].should.equal('test static');
+      result['test2.file'].should.equal('a!test!stream!');
+      done();
+    });
+
+    part1.append('test static', { name: 'test1.file' });
+    part1.finalize();
+
+    part2.append(
+      PartialPipe.of(
+        fromChunks([ 'a', 'test', 'stream' ]),
+        new Transform({ transform(b, _, done) { done(null, b + '!'); } })),
+      { name: 'test2.file' }
+    );
+    part2.finalize();
+  });
+
+  it('should raise one error on the archive if an intermediate PartialPiped data stream errors', (done) => {
+    const part1 = zipPart();
+    const part2 = zipPart();
+
+    const archive = zipStreamFromParts(part1, part2);
+    archive.on('error', (err) => {
+      err.message.should.equal('whoops');
+      done();
+    });
+
+    part1.append('test static', { name: 'test1.file' });
+    part1.finalize();
+
+    part2.append(
+      PartialPipe.of(
+        fromChunks([ 'a', 'test', 'stream' ]),
+        new Transform({ transform(b, _, done) {
+          if (b.length > 4) done(new Error('whoops'));
+          else done(null, b + '!');
+        } }),
+        new Transform({ transform(b, _, done) { done(null, b); } })),
+      { name: 'test2.file' }
+    );
+    part2.finalize();
+  });
+
+  it('should raise one error on the archive if a final PartialPiped data stream errors', (done) => {
+    const part1 = zipPart();
+    const part2 = zipPart();
+
+    const archive = zipStreamFromParts(part1, part2);
+    archive.on('error', (err) => {
+      err.message.should.equal('whoops');
+      done();
+    });
+
+    part1.append('test static', { name: 'test1.file' });
+    part1.finalize();
+
+    part2.append(
+      PartialPipe.of(
+        fromChunks([ 'a', 'test', 'stream' ]),
+        new Transform({ transform(b, _, done) {
+          if (b.length > 4) done(new Error('whoops'));
+          else done(null, b + '!');
+        } })),
+      { name: 'test2.file' }
+    );
     part2.finalize();
   });
 });
