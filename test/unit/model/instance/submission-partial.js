804e768e991df1db98ccabf50e4b8d719421d6ec
Issa Tseng <maintainer@heyissa.com>
2020-02-20 00:34:44 -0800

    bug: iteratee xml parser needs to glue sibling text nodes.

--- test/unit/model/instance/submission-partial.js
+++ test/unit/model/instance/submission-partial.js
@@ -1,6 +1,7 @@
 const should = require('should');
 const Option = require('../../../../lib/util/option');
 const { SubmissionPartial } = require('../../../../lib/model/package').withDefaults();
+const streamTest = require('streamtest').v2;
 
 describe('SubmissionPartial', () => {
   describe('fromXml', () => {
@@ -63,6 +64,13 @@ describe('SubmissionPartial', () => {
         partial.xmlFormId.should.equal('mycoolform');
         partial.instanceId.should.equal('myinstance');
         partial.xml.should.equal(xml);
+      });
+    });
+
+    it('should find the appropriate localKey even if the chunk splits', () => {
+      const xml = streamTest.fromChunks([ '<data id="build_basic_1582152194" version="[encrypted:AXBggUDR]" encrypted="yes" xmlns="http://www.opendatakit.org/xforms/encrypted"><base64EncryptedKey>BGCUNCNPLPFSYOO8lKWsVROLlVDqF8CVxcC5zTQAbByA2f9o/24iUMpPCLCWEf05Rv9wTB17k2QdKaaUaK1ry0FcQJ46OzVAv+snjwaDcbZMB+5EM/mxZY/OtHxhjD+CIpsTHK6VtImW5VZGrWtKQ0TooTivBWo1YKb05gHNRhAy9wksdUAP+7I', 'Y3daKQ+SCitBr8g9Fa8Zt0oeRbzxGP3Uo2u3PBH0YZdeUVPjLgGpYYnSbLeX1I+3AgDw6xobYCnlMlFd4vC2ZHGEgYNz9/syg4xiYRbzjElFFqrEqvKbBySymfbF3RoblhwXFWai3u+YApCYuJup6IwYvGNKRAQ==</base64EncryptedKey><orx:meta xmlns:orx="http://openrosa.org/xforms"><orx:instanceID></orx:instanceID></orx:meta><media><file>u4cnPKejqTlUpQGn.png.enc</file></media><encryptedXmlFile>submission.xml.enc</encryptedXmlFile><base64EncryptedElementSignature>u+OhZZZ9piU/+hqncjavZT5aPaI/dvmX3I7LBw8nq0NQ+1MDOmS37P+/YYBBLdNHA2edkm3k0rjkzSd7ggGlTOW2z3Gn1L5U3tnyIFlRcsvKqYF90x5LFZLjrJ52Gbu51Xplal8tlj1cq3TVRkHv+nvoD1YdKVSglb/uVMkkRL9CVAeJtFujrbph5DcTJrb58QTvP87+b8atEaGfBOedCacGcfjbHAxzKKc8c0CPPjlXtCLgvOGXmQtZNdjF/Y2SP1D5f9J2CGkR9rxg9FXBuwoh3DVwObnRZc3dPvmxGiGw9B502jIuZgp38qK0IM9hjgvT7/hzYdoCSqi/STIo4A==</base64EncryptedElementSignature></data>' ]);
+      return SubmissionPartial.fromXml(xml).then((partial) => {
+        partial.localKey.should.equal('BGCUNCNPLPFSYOO8lKWsVROLlVDqF8CVxcC5zTQAbByA2f9o/24iUMpPCLCWEf05Rv9wTB17k2QdKaaUaK1ry0FcQJ46OzVAv+snjwaDcbZMB+5EM/mxZY/OtHxhjD+CIpsTHK6VtImW5VZGrWtKQ0TooTivBWo1YKb05gHNRhAy9wksdUAP+7IY3daKQ+SCitBr8g9Fa8Zt0oeRbzxGP3Uo2u3PBH0YZdeUVPjLgGpYYnSbLeX1I+3AgDw6xobYCnlMlFd4vC2ZHGEgYNz9/syg4xiYRbzjElFFqrEqvKbBySymfbF3RoblhwXFWai3u+YApCYuJup6IwYvGNKRAQ==');
       });
     });
   });
