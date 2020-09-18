e734682a2433dc41938b33e08f69c28da0aef3cb
Issa Tseng <maintainer@heyissa.com>
2020-09-18 12:46:00 -0500

    bug: possible transaction override in enketo worker; needed for update.
    * actually all updates need for update, apparently, but we'll deal with
      that later.

--- test/util/enketo.js
+++ test/util/enketo.js
@@ -1,4 +1,5 @@
 const appRoot = require('app-root-path');
+const { call } = require('ramda');
 const Problem = require(appRoot + '/lib/util/problem');
 
 const _create = (prefix) => (openRosaUrl, xmlFormId, token) => new Promise((resolve, reject) => {
@@ -7,14 +8,18 @@ const _create = (prefix) => (openRosaUrl, xmlFormId, token) => new Promise((reso
   const token = global.enketoToken || `${prefix}abcdefgh`;
   global.enketoToken = null;
   global.enketoReceivedUrl = null;
+  const wait = global.enketoWait || call;
+  global.enketoWait = null;
 
-  if (state === 'error') {
-    // pretend that Enketo has misbehaved
-    reject(Problem.internal.enketoUnexpectedResponse('wrong status code'));
-  } else {
-    global.enketoReceivedUrl = openRosaUrl;
-    resolve(token);
-  }
+  wait(() => {
+    if (state === 'error') {
+      // pretend that Enketo has misbehaved
+      reject(Problem.internal.enketoUnexpectedResponse('wrong status code'));
+    } else {
+      global.enketoReceivedUrl = openRosaUrl;
+      resolve(token);
+    }
+  });
 });
 
 module.exports = { create: _create('::'), createOnceToken: _create('::::') };
