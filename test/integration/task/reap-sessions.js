e285b8410cbf1cb7d1ad4cf98a0cc2e9cb70f93b
Issa Tseng <maintainer@heyissa.com>
2021-01-29 17:35:07 -0800

    wip: began reworking instances; removed most of the trivial instances.

--- test/integration/task/reap-sessions.js
+++ test/integration/task/reap-sessions.js
@@ -2,14 +2,15 @@ const appRoot = require('app-root-path');
 const should = require('should');
 const { testTask } = require('../setup');
 const { reapSessions } = require(appRoot + '/lib/task/account');
+const { Actor, Session } = require(appRoot + '/lib/model/frames');
 
 describe('task: reap-sessions', () => {
-  it('should remove expired sessions', testTask(({ simply, Actor, Session }) =>
-    (new Actor({ displayName: 'actor', type: 'actor' })).create()
+  it('should remove expired sessions', testTask(({ oneFirst, Actors, Sessions }) =>
+    Actors.create(new Actor({ displayName: 'actor', type: 'actor' }))
       .then((actor) => Promise.all([ 2000, 2001, 2002, 2003, 3000, 3001, 3002, 3003 ]
-        .map((year) => Session.fromActor(actor).with({ expiresAt: `${year}-01-01` }).create())))
-      .then(() => Session.reap())
-      .then(() => simply.countWhere('sessions'))
+        .map((year) => Sessions.create(actor, `${year}-01-01` }))
+      .then(() => Sessions.reap())
+      .then(() => oneFirst(sql`select count(*) from sessions`))
       .then((count) => { count.should.equal(4); })));
 });
 
