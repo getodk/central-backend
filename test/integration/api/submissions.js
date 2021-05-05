7242fbca50aa85489fa35263cefaf3d137802119
Issa Tseng <maintainer@heyissa.com>
2021-05-05 16:54:35 -0700

    bug: fix null odata literals so received review state filter works.

--- test/integration/api/submissions.js
+++ test/integration/api/submissions.js
@@ -1271,6 +1271,33 @@ describe('api: /forms/:id/submissions', () => {
                 done();
               })))))));
 
+    it('should return a review state-filtered zipfile with the relevant data', testService((service) =>
+      service.login('alice', (asAlice) =>
+        asAlice.post('/v1/projects/1/forms/simple/submissions')
+          .send(testData.instances.simple.one)
+          .set('Content-Type', 'text/xml')
+          .expect(200)
+          .then(() => asAlice.post('/v1/projects/1/forms/simple/submissions')
+            .send(testData.instances.simple.two)
+            .set('Content-Type', 'text/xml')
+            .expect(200))
+          .then(() => asAlice.patch('/v1/projects/1/forms/simple/submissions/two')
+            .send({ reviewState: 'approved' })
+            .expect(200))
+          .then(() => asAlice.post('/v1/projects/1/forms/simple/submissions')
+            .send(testData.instances.simple.three)
+            .set('Content-Type', 'text/xml')
+            .expect(200))
+          .then(() => new Promise((done) =>
+            zipStreamToFiles(asAlice.get('/v1/projects/1/forms/simple/submissions.csv.zip?$filter=__system/reviewState eq null'), (result) => {
+              result.filenames.should.eql([ 'simple.csv' ]);
+              const lines = result['simple.csv'].split('\n');
+              lines.length.should.equal(4);
+              lines[1].endsWith(',three,Chelsea,38,three,5,Alice,0,0').should.equal(true);
+              lines[2].endsWith(',one,Alice,30,one,5,Alice,0,0').should.equal(true);
+              done();
+            }))))));
+
     it('should return a submissionDate-filtered zipfile with the relevant data', testService((service, { run }) =>
       service.login('alice', (asAlice) =>
         service.login('bob', (asBob) =>
