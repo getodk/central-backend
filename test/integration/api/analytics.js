9e5b753197fc7c0e0c9d5dc01df303088079cd08
Kathleen Tuite <kathleen.tuite@gmail.com>
2021-09-09 13:29:32 -0700

    Test analytics preview resource

---
+++ test/integration/api/analytics.js
@@ -0,0 +1,23 @@
+const appRoot = require('app-root-path');
+const should = require('should');
+const { map, sortBy } = require('ramda');
+const { getOrNotFound } = require(appRoot + '/lib/util/promise');
+const { testService } = require('../setup');
+
+describe.only('api: /analytics/preview', () => {
+  describe('GET', () => {
+    it('should reject if the user cannot read analytics', testService((service) =>
+      service.login('chelsea', (asChelsea) =>
+        asChelsea.get('/v1/analytics/preview').expect(403))));
+
+    it('should return the analytics', testService((service) =>
+      service.login('alice', (asAlice) =>
+        asAlice.get('/v1/analytics/preview')
+          .expect(200)
+          .then(({ body }) => {
+            body.system.num_admins.recent.should.equal(1);
+            body.projects.length.should.equal(1);
+          }))));
+  });
+});
+
