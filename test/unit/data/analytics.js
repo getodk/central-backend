e060fc3e4b94b2b5a832c567702d7f062a8c0237
Kathleen Tuite <kathleen.tuite@gmail.com>
2021-09-22 17:02:05 -0700

    Simplified analytics unit tests

--- test/unit/data/analytics.js
+++ test/unit/data/analytics.js
@@ -27,8 +27,8 @@ describe('analytics data', () => {
   });
 
   it('should make a meta/uuid segment of xml', () => {
-    const xml = metaWithUuidXml().replace(/<instanceID>(.*)<\/instanceID>/, '<instanceID>1234</instanceID>');
-    xml.should.equal('<meta><instanceID>1234</instanceID></meta>');
+    const xml = metaWithUuidXml();
+    xml.should.match(/<meta><instanceID>uuid:.*<\/instanceID><\/meta>/);
   });
 
   it('should build a valid xml submission that central can parse', () => {
@@ -36,12 +36,10 @@ describe('analytics data', () => {
     const formVersion = '123';
     const config = { email: 'test@getodk.org', organization: 'ODK' };
     const xml = buildSubmission(formId, formVersion, data, config);
-    const expected = '<?xml version="1.0"?><data xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms" id="the-form-id" version="123"><system><num_admins><recent>11</recent><total>22</total></num_admins></system><projects><id>123</id><users><num_managers><recent>2</recent><total>2</total></num_managers></users></projects><projects><id>456</id><users><num_managers><recent>3</recent><total>3</total></num_managers></users></projects><config><email>test@getodk.org</email><organization>ODK</organization></config><meta><instanceID>1234</instanceID></meta></data>';
 
     return Submission.fromXml(xml).then((partial) => {
       partial.xmlFormId.should.equal('the-form-id');
       partial.instanceId.length.should.equal(41);
-      partial.xml.replace(/<instanceID>(.*)<\/instanceID>/, '<instanceID>1234</instanceID>').should.equal(expected);
     });
   });
 
@@ -50,8 +48,8 @@ describe('analytics data', () => {
     const formVersion = '123';
     const config = {};
     const simpleData = { num_admins: 1 };
-    const xml = buildSubmission(formId, formVersion, simpleData, config).replace(/<instanceID>(.*)<\/instanceID>/, '<instanceID>1234</instanceID>');
-    xml.should.equal('<?xml version="1.0"?><data xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms" id="the-form-id" version="123"><num_admins>1</num_admins><config></config><meta><instanceID>1234</instanceID></meta></data>');
+    const xml = buildSubmission(formId, formVersion, simpleData, config);
+    xml.includes('<config></config>').should.equal(true);
   });
 
   it('should build xml when config is email only, no organization', () => {
@@ -59,8 +57,27 @@ describe('analytics data', () => {
     const formVersion = '123';
     const config = { email: 'test@getodk.org' };
     const simpleData = { num_admins: 1 };
-    const xml = buildSubmission(formId, formVersion, simpleData, config).replace(/<instanceID>(.*)<\/instanceID>/, '<instanceID>1234</instanceID>');
-    xml.should.equal('<?xml version="1.0"?><data xmlns:jr="http://openrosa.org/javarosa" xmlns:orx="http://openrosa.org/xforms" id="the-form-id" version="123"><num_admins>1</num_admins><config><email>test@getodk.org</email></config><meta><instanceID>1234</instanceID></meta></data>');
+    const xml = buildSubmission(formId, formVersion, simpleData, config);
+    xml.includes('<config><email>test@getodk.org</email></config>').should.equal(true);
+  });
+
+  it('should build xml with full config info', () => {
+    const formId = 'the-form-id';
+    const formVersion = '123';
+    const config = { email: 'test@getodk.org', organization: "ODK" };
+    const simpleData = { num_admins: 1 };
+    const xml = buildSubmission(formId, formVersion, simpleData, config);
+    xml.includes('<config><email>test@getodk.org</email><organization>ODK</organization></config>').should.equal(true);
+  });
+
+
+  it('should build xml without email and org if they are undefined', () => {
+    const formId = 'the-form-id';
+    const formVersion = '123';
+    const config = { email: undefined, organization: undefined };
+    const simpleData = { num_admins: 1 };
+    const xml = buildSubmission(formId, formVersion, simpleData, config);
+    xml.includes('<config></config>').should.equal(true);
   });
 });
 
