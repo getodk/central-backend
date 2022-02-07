60e29c67c28eb616101bad8c15eedab370b92a2e
Kathleen Tuite <kathleen.tuite@gmail.com>
2022-02-07 11:05:11 -0800

    Purge migration xlsblobid bug fixed

--- test/integration/other/migrations.js
+++ test/integration/other/migrations.js
@@ -1,3 +1,4 @@
+const { readFileSync } = require('fs');
 const appRoot = require('app-root-path');
 const should = require('should');
 const config = require('config');
@@ -38,9 +39,70 @@ describe('database migrations', function() {
       asAlice.delete('/v1/projects/1/forms/simple')
         .expect(200));
 
+    // running migration 20220121-02-purge-deleted-forms.js
     await migrator.migrate.up({ directory: appRoot + '/lib/model/migrations' });
 
     const count = await container.oneFirst(sql`select count(*) from forms`);
     count.should.equal(1); // only the withrepeat base test should exist
+  }));
+
+  it('should not purge blobs that are still referenced', testServiceFullTrx(async (service, container) => {
+    // An earlier version of this migration [20220121-02-purge-deleted-forms.js]
+    // failed because it tried to purge blobs that were still being used as
+    // xlsBlobIds on active form definitons.
+    await upToMigration('20220121-01-form-cascade-delete.js');
+    await populateUsers(container);
+    await populateForms(container);
+
+    await service.login('alice', (asAlice) =>
+      asAlice.post('/v1/projects/1/forms?publish=true')
+        .send(readFileSync(appRoot + '/test/data/simple.xlsx'))
+        .set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        .expect(200));
+
+    // running migration 20220121-02-purge-deleted-forms.js
+    await migrator.migrate.up({ directory: appRoot + '/lib/model/migrations' });
+
+    const count = await container.oneFirst(sql`select count(*) from blobs`);
+    count.should.equal(1); // the xls blob should still exist
+  }));
+
+  it('should purge blobs of deleted forms', testServiceFullTrx(async (service, container) => {
+    // An earlier version of this migration [20220121-02-purge-deleted-forms.js]
+    // failed because it tried to purge blobs that were still being used as
+    // xlsBlobIds on active form definitons.
+    await upToMigration('20220121-01-form-cascade-delete.js');
+    await populateUsers(container);
+    await populateForms(container);
+
+    // xmlFormId of this xlsx form is 'simple2'
+    await service.login('alice', (asAlice) =>
+      asAlice.post('/v1/projects/1/forms?publish=true')
+        .send(readFileSync(appRoot + '/test/data/simple.xlsx'))
+        .set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        .expect(200)
+        .then(() => asAlice.delete('/v1/projects/1/forms/simple2') // Delete form
+          .expect(200))
+        .then(() => asAlice.post('/v1/projects/1/forms?publish=true')
+          .set('Content-Type', 'application/xml')
+          .send(testData.forms.binaryType)
+          .expect(200))
+        .then(() => asAlice.post('/v1/projects/1/submission')
+          .set('X-OpenRosa-Version', '1.0')
+          .attach('xml_submission_file', Buffer.from(testData.instances.binaryType.both), { filename: 'data.xml' })
+          .attach('here_is_file2.jpg', Buffer.from('this is test file two'), { filename: 'here_is_file2.jpg' })
+          .attach('my_file1.mp4', Buffer.from('this is test file one'), { filename: 'my_file1.mp4' })
+          .expect(201))
+        .then(() => asAlice.delete('/v1/projects/1/forms/binaryType') // Delete form
+          .expect(200)));
+
+    let count = await container.oneFirst(sql`select count(*) from blobs`);
+    count.should.equal(3); // xls blob and two file blobs
+
+    // running migration 20220121-02-purge-deleted-forms.js
+    await migrator.migrate.up({ directory: appRoot + '/lib/model/migrations' });
+
+    count = await container.oneFirst(sql`select count(*) from blobs`);
+    count.should.equal(0); // blobs should all be purged
   }));
 });
