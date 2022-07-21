8377a0f1f62607098148af77a939f2cce3710994
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2022-07-22 00:23:39 +0300

    Allow submission edits on closed forms (#530)
    * openRosaSubmission(): replace promise chain with async/await
    
    * Allow submission edit saving on closed forms
    
    * Convert to functional style
    
    * Remove untested code path
    
    * remove obvious comment
    
    * Add new test cases for drafts(??)
    
    * Add space after if
    
    Co-authored-by: alxndrsn <alxndrsn>

--- test/integration/api/submissions.js
+++ test/integration/api/submissions.js
@@ -540,6 +540,42 @@ describe('api: /submission', () => {
                 body.value[0].name.should.equal('Alyssa');
               })))));
 
+      it('should accept submission update for a closing form', testService((service) =>
+        service.login('alice', (asAlice) =>
+          asAlice.post('/v1/projects/1/submission')
+            .set('X-OpenRosa-Version', '1.0')
+            .attach('xml_submission_file', Buffer.from(testData.instances.simple.one), { filename: 'data.xml' })
+            .expect(201)
+            .then(() => asAlice.patch('/v1/projects/1/forms/simple')
+              .send({ state: 'closing' })
+              .expect(200))
+            .then(() => asAlice.post('/v1/projects/1/submission')
+              .set('X-OpenRosa-Version', '1.0')
+              .attach('xml_submission_file', Buffer.from(withSimpleIds('one', 'two').replace('Alice', 'Alyssa')), { filename: 'data.xml' })
+              .expect(201))
+            .then(() => asAlice.get('/v1/projects/1/forms/simple.svc/Submissions')
+              .then(({ body }) => {
+                body.value[0].name.should.equal('Alyssa');
+              })))));
+
+      it('should accept submission update for a closed form', testService((service) =>
+        service.login('alice', (asAlice) =>
+          asAlice.post('/v1/projects/1/submission')
+            .set('X-OpenRosa-Version', '1.0')
+            .attach('xml_submission_file', Buffer.from(testData.instances.simple.one), { filename: 'data.xml' })
+            .expect(201)
+            .then(() => asAlice.patch('/v1/projects/1/forms/simple')
+              .send({ state: 'closed' })
+              .expect(200))
+            .then(() => asAlice.post('/v1/projects/1/submission')
+              .set('X-OpenRosa-Version', '1.0')
+              .attach('xml_submission_file', Buffer.from(withSimpleIds('one', 'two').replace('Alice', 'Alyssa')), { filename: 'data.xml' })
+              .expect(201))
+            .then(() => asAlice.get('/v1/projects/1/forms/simple.svc/Submissions')
+              .then(({ body }) => {
+                body.value[0].name.should.equal('Alyssa');
+              })))));
+
       it('should set the submission review state to edited', testService((service) =>
         service.login('alice', (asAlice) =>
           asAlice.post('/v1/projects/1/submission')
@@ -650,6 +686,62 @@ describe('api: /submission', () => {
       service.login('alice', (asAlice) =>
         asAlice.post('/v1/projects/1/forms/simple/draft')
           .expect(200)
+          .then(() => asAlice.post('/v1/projects/1/forms/simple/draft/submission')
+            .set('X-OpenRosa-Version', '1.0')
+            .attach('xml_submission_file', Buffer.from(testData.instances.simple.one), { filename: 'data.xml' })
+            .expect(201)
+            .then(({ text }) => {
+              text.should.match(/upload was successful/);
+            })
+            .then(() => Promise.all([
+              asAlice.get('/v1/projects/1/forms/simple/draft/submissions/one')
+                .expect(200)
+                .then(({ body }) => {
+                  body.createdAt.should.be.a.recentIsoDate();
+                  should.not.exist(body.deviceId);
+                }),
+              asAlice.get('/v1/projects/1/forms/simple/draft/submissions/one.xml')
+                .expect(200)
+                .then(({ text }) => { text.should.equal(testData.instances.simple.one); }),
+              asAlice.get('/v1/projects/1/forms/simple/submissions/one')
+                .expect(404)
+            ]))))));
+
+    it('should save a submission for the draft of a closing form', testService((service) =>
+      service.login('alice', (asAlice) =>
+        asAlice.patch('/v1/projects/1/forms/simple')
+          .send({ state: 'closing' })
+          .expect(200)
+          .then(() => asAlice.post('/v1/projects/1/forms/simple/draft')
+            .expect(200))
+          .then(() => asAlice.post('/v1/projects/1/forms/simple/draft/submission')
+            .set('X-OpenRosa-Version', '1.0')
+            .attach('xml_submission_file', Buffer.from(testData.instances.simple.one), { filename: 'data.xml' })
+            .expect(201)
+            .then(({ text }) => {
+              text.should.match(/upload was successful/);
+            })
+            .then(() => Promise.all([
+              asAlice.get('/v1/projects/1/forms/simple/draft/submissions/one')
+                .expect(200)
+                .then(({ body }) => {
+                  body.createdAt.should.be.a.recentIsoDate();
+                  should.not.exist(body.deviceId);
+                }),
+              asAlice.get('/v1/projects/1/forms/simple/draft/submissions/one.xml')
+                .expect(200)
+                .then(({ text }) => { text.should.equal(testData.instances.simple.one); }),
+              asAlice.get('/v1/projects/1/forms/simple/submissions/one')
+                .expect(404)
+            ]))))));
+
+    it('should save a submission for the draft of a closed form', testService((service) =>
+      service.login('alice', (asAlice) =>
+        asAlice.patch('/v1/projects/1/forms/simple')
+          .send({ state: 'closed' })
+          .expect(200)
+          .then(() => asAlice.post('/v1/projects/1/forms/simple/draft')
+            .expect(200))
           .then(() => asAlice.post('/v1/projects/1/forms/simple/draft/submission')
             .set('X-OpenRosa-Version', '1.0')
             .attach('xml_submission_file', Buffer.from(testData.instances.simple.one), { filename: 'data.xml' })
