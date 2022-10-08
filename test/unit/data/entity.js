90c92c941697fe69616644ee326934be22fbc39d
Kathleen Tuite <kathleen.tuite@gmail.com>
2022-10-07 21:48:12 -0700

    Rename entity parsing function

--- test/unit/data/entity.js
+++ test/unit/data/entity.js
@@ -1,18 +1,18 @@
 require('should');
 const appRoot = require('app-root-path');
 // eslint-disable-next-line import/no-dynamic-require
-const { getEntity } = require(appRoot + '/lib/data/entity');
+const { parseSubmissionXml } = require(appRoot + '/lib/data/entity');
 // eslint-disable-next-line import/no-dynamic-require
 const { fieldsFor } = require(appRoot + '/test/util/schema');
 // eslint-disable-next-line import/no-dynamic-require
 const testData = require(appRoot + '/test/data/xml');
 
 
-describe('getEntity', () => {
+describe('parseSubmissionXml', () => {
   it('should return filled in entity props', () =>
     fieldsFor(testData.forms.simpleEntity)
       .then((fields) => fields.filter((field) => field.propertyName || field.path.indexOf('/meta/entity') === 0))
-      .then((fields) => getEntity(fields, testData.instances.simpleEntity.one))
+      .then((fields) => parseSubmissionXml(fields, testData.instances.simpleEntity.one))
       .then((result) => {
         result.data.should.eql({ first_name: 'Alice', age: '88' });
         result.system.should.eql({
