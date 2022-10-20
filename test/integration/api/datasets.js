bf14eb5ec87b531856e00f0a2c4cae1f89e7f597
Sadiq Khoja <sadiqkhoja@gmail.com>
2022-10-20 12:24:12 -0400

    new: download dataset csv as attachment (#640)
    * new: download dataset csv as attachment
    fix: use projectId to get dataset-diff\

--- test/integration/api/datasets.js
+++ test/integration/api/datasets.js
@@ -1,9 +1,12 @@
+const appRoot = require('app-root-path');
 const { testService } = require('../setup');
 const testData = require('../../data/xml');
 const config = require('config');
 const { Form } = require('../../../lib/model/frames');
 const { getOrNotFound } = require('../../../lib/util/promise');
 const { omit } = require('ramda');
+// eslint-disable-next-line import/no-dynamic-require
+const { createEntityFromSubmission } = require(appRoot + '/lib/worker/entity');
 
 // TODO merge with test/integration/api/forms/dataset.js
 
@@ -273,4 +276,36 @@ describe('Check only blobId or datasetId is set', () => {
             .catch(error => {
               error.constraint.should.be.equal('check_blobId_or_datasetId_is_null');
             }))))));
+});
+
+describe('projects/:id/forms/:formId/attachments/:name (entities dataset)', () => {
+  it('should return entities csv', testService((service, container) =>
+    service.login('alice', (asAlice) =>
+      asAlice.post('/v1/projects/1/forms')
+        .send(testData.forms.withAttachments)
+        .set('Content-Type', 'application/xml')
+        .expect(200)
+        .then(() => asAlice.post('/v1/projects/1/forms?publish=true')
+          .send(testData.forms.simpleEntity.replace(/people/g, 'goodone'))
+          .set('Content-Type', 'application/xml')
+          .expect(200))
+        .then(() => asAlice.post('/v1/projects/1/forms/simpleEntity/submissions')
+          .send(testData.instances.simpleEntity.one.replace(/people/g, 'goodone'))
+          .set('Content-Type', 'application/xml')
+          .expect(200))
+        .then(() => container.Submissions.getCurrentDefByIds(1, 'simpleEntity', 'one', false)
+          .then(getOrNotFound)
+          .then((subDef) => createEntityFromSubmission(container, { details: { submissionDefId: subDef.id, reviewState: 'approved' } })))
+        .then(() => asAlice.patch('/v1/projects/1/forms/withAttachments/draft/attachments/goodone.csv')
+          .send({ dataset: true })
+          .expect(200))
+        .then(() => asAlice.post('/v1/projects/1/forms/withAttachments/draft/publish')
+          .expect(200))
+        .then(() => asAlice.get('/v1/projects/1/forms/withAttachments/attachments/goodone.csv')
+          .expect(200)
+          .then(({ headers, text }) => {
+            headers['content-disposition'].should.equal('attachment; filename="goodone.csv"; filename*=UTF-8\'\'goodone.csv');
+            headers['content-type'].should.equal('text/csv; charset=utf-8');
+            text.should.equal('name,label,name,age\nuuid:12345678-1234-1234-1234-123456789abc,Alice (88),Alice,88\n');
+          })))));
 });
