ff653c360c805635285825bd05afe068a347abeb
Sadiq Khoja <sadiqkhoja@gmail.com>
2022-11-21 15:27:48 -0500

    refactored entities feature: removed unnecessary readables writables removed duplicate tests renamed CTE expressions removed  from makeHierarchy changed {} with Object.create added deletedAt is null to getDatasetDiff sql projects/:id/datasets doesn't return revisionNumber

--- test/integration/api/projects.js
+++ test/integration/api/projects.js
@@ -1286,43 +1286,6 @@ describe('api: /projects', () => {
                 .then((o) => { o.isDefined().should.equal(false); })
             ])))))));
   });
-
-  describe('/:id/datasets GET', () => {
-    it('should return the datasets of Default project', testService((service) =>
-      service.login('alice', (asAlice) =>
-        asAlice.post('/v1/projects/1/forms?publish=true')
-          .send(testData.forms.simpleEntity)
-          .set('Content-Type', 'application/xml')
-          .expect(200)
-          .then(() =>
-            asAlice.get('/v1/projects/1/datasets')
-              .expect(200)
-              .then(({ body }) => {
-                body.map(({ id, createdAt, ...d }) => d).should.eql([
-                  { name: 'people', projectId: 1, revisionNumber: 0 }
-                ]);
-              })))));
-
-    it('should not return draft datasets', testService((service) =>
-      service.login('alice', (asAlice) =>
-        asAlice.post('/v1/projects/1/forms')
-          .send(testData.forms.simpleEntity)
-          .set('Content-Type', 'application/xml')
-          .expect(200)
-          .then(() => asAlice.post('/v1/projects/1/forms?publish=true')
-            .send(testData.forms.simpleEntity
-              .replace(/simpleEntity/, 'simpleEntity2')
-              .replace(/people/, 'student'))
-            .expect(200)
-            .then(() =>
-              asAlice.get('/v1/projects/1/datasets')
-                .expect(200)
-                .then(({ body }) => {
-                  body.map(({ id, createdAt, ...d }) => d).should.eql([
-                    { name: 'student', projectId: 1, revisionNumber: 0 }
-                  ]);
-                }))))));
-  });
 });
 
 // Nested extended forms
