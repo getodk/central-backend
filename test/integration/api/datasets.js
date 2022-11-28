4a161b40379f68d72d78acd09def7384d58832da
Sadiq Khoja <sadiqkhoja@gmail.com>
2022-11-28 12:21:33 -0500

    fix: dataset-diff should return dataset even if there's no properties

--- test/integration/api/datasets.js
+++ test/integration/api/datasets.js
@@ -813,6 +813,26 @@ describe('datasets and entities', () => {
                   }]);
                 }))));
       }));
+
+      it('should return dataset name only if there is no properties', testService(async (service) => {
+        const asAlice = await service.login('alice', identity);
+
+        await asAlice.post('/v1/projects/1/forms?publish=true')
+          .send(testData.forms.simpleEntity.replace(/entities:saveto[^/]+/g, ''))
+          .set('Content-Type', 'application/xml')
+          .expect(200);
+
+        await asAlice.get('/v1/projects/1/forms/simpleEntity/dataset-diff')
+          .expect(200)
+          .then(({ body }) => {
+            body.should.be.eql([{
+              name: 'people',
+              properties: []
+            }]);
+          });
+
+      }));
+
     });
   });
 
