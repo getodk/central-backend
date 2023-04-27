d672a38ac6df8d0fba6d699dade24f43d40ed56e
alxndrsn <alxndrsn>
2023-04-27 06:22:10 +0000

    Make Authorization header checks tighter
    Include trailing space in checks for Basic and Bearer prefixes in
    Authorization headers.  This space is already assumed to exist in
    subsequent parsing code.
    
    An extra check is added for requests which _provide_ the Authorization
    header, but in an unrecognised format.  This ensures that a 401 header
    is still sent in preference to 404, as per #329.

--- test/integration/other/bearer-auth.js
+++ test/integration/other/bearer-auth.js
@@ -1,7 +1,7 @@
 const { testService } = require('../setup');
 
 describe('bearer authentication', () => {
-  it('should reject 401 if the key format is wrong', testService((service) => // gh329
+  it('should reject 401 if the header format is wrong', testService((service) => // gh329
     service.get('/v1/users/current')
       .set('Authorization', 'Bearer: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')
       .expect(401)));
