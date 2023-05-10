cd23ead3b444730aad0d5720af6fdf6efe8dd4e0
Kathleen Tuite <kathleen.tuite@gmail.com>
2023-05-10 10:25:53 -0700

    Make entity update audit details consistent with create details (#866)
    * Make entity update audit details consistent with create details
    
    * Fixed another test

--- test/integration/api/entities.js
+++ test/integration/api/entities.js
@@ -372,7 +372,7 @@ describe('Entities API', () => {
         .then(({ body: logs }) => {
           logs[0].should.be.an.Audit();
           logs[0].action.should.be.eql('entity.update.version');
-          logs[0].details.uuid.should.be.eql('12345678-1234-4123-8234-123456789abc');
+          logs[0].details.entity.uuid.should.be.eql('12345678-1234-4123-8234-123456789abc');
           logs[0].actor.displayName.should.be.eql('Bob');
 
           logs[1].should.be.an.Audit();
@@ -978,8 +978,8 @@ describe('Entities API', () => {
 
       const audit = await container.Audits.getLatestByAction('entity.update.version').then(a => a.get());
       audit.actorId.should.equal(6);
-      audit.details.uuid.should.eql('12345678-1234-4123-8234-123456789abc');
-      audit.details.dataset.should.eql('people');
+      audit.details.entity.uuid.should.eql('12345678-1234-4123-8234-123456789abc');
+      audit.details.entity.dataset.should.eql('people');
     }));
   });
 
