8c0ca0c5a42327f39fa432e76ecdb30c9ac46fe7
alxndrsn <alxndrsn>
2023-11-03 13:46:02 +0000

    revert changes; remove unuseful tests

--- test/unit/util/html.js
+++ test/unit/util/html.js
@@ -26,21 +26,18 @@ describe('util/html', () => {
       [ '/users?"=1#"=><badTag ', '/#/users?%22=1#%22=%3E%3CbadTag' ], // eslint-disable-line no-multi-spaces
 
       // enketo-express
-      [ '/-/xyz',                          'http://localhost:8989/-/xyz' ],                       // eslint-disable-line no-multi-spaces
-      [ '/-/xyz?"><b',                     'http://localhost:8989/-/xyz?%22%3E%3Cb' ],            // eslint-disable-line no-multi-spaces
-      [ '/-/xyz#"><b',                     'http://localhost:8989/-/xyz#%22%3E%3Cb' ],            // eslint-disable-line no-multi-spaces
-      [ '/-/xyz?"><b#"><b',                'http://localhost:8989/-/xyz?%22%3E%3Cb#%22%3E%3Cb' ], // eslint-disable-line no-multi-spaces
+      [ '/-/xyz',                 'http://localhost:8989/-/xyz' ],                       // eslint-disable-line no-multi-spaces
+      [ '/-/xyz?"><b',            'http://localhost:8989/-/xyz?%22%3E%3Cb' ],            // eslint-disable-line no-multi-spaces
+      [ '/-/xyz#"><b',            'http://localhost:8989/-/xyz#%22%3E%3Cb' ],            // eslint-disable-line no-multi-spaces
+      [ '/-/xyz?"><b#"><b',       'http://localhost:8989/-/xyz?%22%3E%3Cb#%22%3E%3Cb' ], // eslint-disable-line no-multi-spaces
       // with path traversal
-      [ '/-/../version.txt',               '/#/version.txt' ],                                    // eslint-disable-line no-multi-spaces
-      [ '/-/&period;&period;/version.txt', '/#/version.txt' ],                                    // eslint-disable-line no-multi-spaces
+      [ '/-/../version.txt',      '/#/version.txt' ],                                    // eslint-disable-line no-multi-spaces
 
       // bad domain
       [ 'http://example.com',                  '/#/' ], // eslint-disable-line no-multi-spaces
       // with @ char - not a problem if positioned in fragment or after first `/`:
       [ '@baddomain.com',                      '/#/@baddomain.com' ],                         // eslint-disable-line no-multi-spaces
       [ '/-/@baddomain.com',                   'http://localhost:8989/-/@baddomain.com' ],    // eslint-disable-line no-multi-spaces
-      [ '&64;baddomain.com',                   '/#/&64;baddomain.com' ],                      // eslint-disable-line no-multi-spaces
-      [ '/-/&64;baddomain.com',                'http://localhost:8989/-/&64;baddomain.com' ], // eslint-disable-line no-multi-spaces
       [ 'http://localhost:8989@baddomain.com', '/#/' ],                                       // eslint-disable-line no-multi-spaces
       [ 'http://localhost:8989@baddomain.com', '/#/' ],                                       // eslint-disable-line no-multi-spaces
 
