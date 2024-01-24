e19e16e0abf29a2cf385008ff8630f9f7b04013c
alxndrsn <alxndrsn>
2024-01-24 13:10:15 +0000

    re-add normal test

--- test/integration/other/blobs.js
+++ test/integration/other/blobs.js
@@ -17,7 +17,61 @@ describe('blob query module', () => {
         .then(() => container.oneFirst(sql`select count(*) from blobs`))
         .then((count) => count.should.equal(1)))));
 
-  it('should handle blob collisions with different filenames (mp4 first)', testService((service, container) =>
+  it('should handle blob collisions with different filenames', testService((service, container) =>
+    // One one instance of the form, two files are uploaded
+    // On another instance of the form (different id), one file is uploaded
+    // and it creates another reference to one of the blobs with a different
+    // filename.
+    service.login('alice', (asAlice) =>
+      asAlice.post('/v1/projects/1/forms?publish=true')
+        .set('Content-Type', 'application/xml')
+        .send(testData.forms.binaryType)
+        .expect(200)
+        .then(() => container.oneFirst(sql`select count(*) from blobs`))
+        .then((count) => count.should.equal(0))
+        .then(() => asAlice.post('/v1/projects/1/submission')
+          .set('X-OpenRosa-Version', '1.0')
+          .attach('xml_submission_file', Buffer.from(testData.instances.binaryType.both), { filename: 'data.xml' })
+          .attach('here_is_file2.jpg', Buffer.from('this is test file two'), { filename: 'here_is_file2.jpg' })
+          .attach('my_file1.mp4', Buffer.from('this is test file one'), { filename: 'my_file1.mp4' })
+          .expect(201))
+        .then(() => container.oneFirst(sql`select count(*) from blobs`))
+        .then((count) => count.should.equal(2))
+        .then(() => asAlice.post('/v1/projects/1/forms?publish=true')
+          .set('Content-Type', 'application/xml')
+          .send(testData.forms.binaryType.replace('id="binaryType"', 'id="binaryType2"'))
+          .expect(200))
+        .then(() => asAlice.post('/v1/projects/1/submission')
+          .set('X-OpenRosa-Version', '1.0')
+          .attach(
+            'xml_submission_file',
+            Buffer.from(testData.instances.binaryType.one
+              .replace('id="binaryType"', 'id="binaryType2"')
+              .replace('<file1>my_file1.mp4</file1>', '<file1>my_file2.mp4</file1>')),
+            { filename: 'data.xml' },
+          )
+          .attach('my_file2.mp4', Buffer.from('this is test file one'), { filename: 'my_file2.mp4' })
+          .expect(201))
+        .then(() => container.oneFirst(sql`select count(*) from blobs`))
+        .then((count) => count.should.equal(2))
+        .then(() => asAlice.get('/v1/projects/1/forms/binaryType/submissions/both/attachments/my_file1.mp4')
+          .expect(200)
+          .then(({ headers, body }) => {
+            headers['content-type'].should.equal('video/mp4');
+            headers['content-disposition'].should.equal('attachment; filename="my_file1.mp4"; filename*=UTF-8\'\'my_file1.mp4');
+            headers['etag'].should.equal('"75f5701abfe7de8202cecaa0ca753f29"'); // eslint-disable-line dot-notation
+            body.toString('utf8').should.equal('this is test file one');
+          }))
+        .then(() => asAlice.get('/v1/projects/1/forms/binaryType2/submissions/bone/attachments/my_file2.mp4')
+          .expect(200)
+          .then(({ headers, body }) => {
+            headers['content-type'].should.equal('video/mp4');
+            headers['content-disposition'].should.equal('attachment; filename="my_file2.mp4"; filename*=UTF-8\'\'my_file2.mp4');
+            headers['etag'].should.equal('"75f5701abfe7de8202cecaa0ca753f29"'); // eslint-disable-line dot-notation
+            body.toString('utf8').should.equal('this is test file one');
+          })))));
+
+  it('should handle blob collisions with different mime types (mp4 first)', testService((service, container) =>
     // One one instance of the form, two files are uploaded
     // On another instance of the form (different id), one file is uploaded
     // and it creates another reference to one of the blobs with a different
@@ -87,7 +141,7 @@ describe('blob query module', () => {
             text.toString('utf8').should.equal('this is test file one');
           })))));
 
-  it('should handle blob collisions with different filenames (xlsx first)', testService((service, container) =>
+  it('should handle blob collisions with different mime types (xlsx first)', testService((service, container) =>
     // One one instance of the form, two files are uploaded
     // On another instance of the form (different id), one file is uploaded
     // and it creates another reference to one of the blobs with a different
