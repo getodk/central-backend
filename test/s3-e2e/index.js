24797dcb078aada5bde5f7a32d043af66ec177a4
alxndrsn <alxndrsn>
2024-02-02 07:23:21 +0000

    Test working locally...

--- test/s3-e2e/index.js
+++ test/s3-e2e/index.js
@@ -11,15 +11,22 @@ const fs = require('node:fs');
 const fetch = require('node-fetch');
 const _ = require('lodash');
 const uuid = require('uuid').v4;
+const { randomBytes } = require('node:crypto');
 const { basename } = require('node:path');
 const { program } = require('commander');
+const should = require('should');
 
-const _log = (...args) => console.log(`[${new Date().toISOString()}]`, '[soak-tester]', ...args);
-const log  = (...args) => true  && _log('INFO',   ...args);
-log.debug  = (...args) => false && _log('DEBUG',  ...args);
+const LOG_LEVELS = ['DEBUG', 'INFO', 'WARN', 'ERROR', 'REPORT']
+const logLevel = process.env.LOG_LEVEL || 'INFO';
+const _log = (level, ...args) => {
+  if(LOG_LEVELS.indexOf(logLevel) > LOG_LEVELS.indexOf(level)) return;
+  console.log(`[${new Date().toISOString()}]`, '[s3-e2e-test]', ...args);
+};
+const log  = (...args) => _log('INFO',   ...args);
+log.debug  = (...args) => _log('DEBUG',  ...args);
 log.info   = log;
-log.error  = (...args) => true  && _log('ERROR',  ...args);
-log.report = (...args) => true  && _log('REPORT', ...args);
+log.error  = (...args) => _log('ERROR',  ...args);
+log.report = (...args) => _log('REPORT', ...args);
 
 program
     .option('-s, --server-url <serverUrl>', 'URL of ODK Central server', 'http://localhost:8383')
@@ -29,9 +36,8 @@ program
 program.parse();
 const { serverUrl, userEmail, userPassword } = program.opts();
 
-// TODO create draft form
-// TODO add form attachments
-// TODO attempt to download form attachments
+const attDir = 'test/s3-e2e/test-attachments';
+const BIGFILE = `${attDir}/big.bin`;
 
 let bearerToken;
 
@@ -39,6 +45,17 @@ runTest();
 
 async function runTest() {
   log.info('Setting up...');
+
+  if(fs.existsSync(BIGFILE)) {
+    log.info('big.bin exists; skipping generation');
+  } else {
+    log.info('Generating big.bin...');
+    let remaining = 100_000_000;
+    const batchSize = 100_000;
+    do {
+      fs.appendFileSync(BIGFILE, randomBytes(batchSize));
+    } while((remaining-=batchSize) > 0);
+  }
 
   log.info('Creating session...');
   const { token } = await apiPostJson('sessions', { email:userEmail, password:userPassword }, { Authorization:null });
@@ -48,22 +65,79 @@ async function runTest() {
   const { id:projectId } = await apiPostJson('projects', { name:`soak-test-${new Date().toISOString().replace(/\..*/, '')}` });
 
   log.info('Uploading form...');
-  const { xmlFormId:formId } = await apiPostFile(`projects/${projectId}/forms`, 'test/s3-e2e/test-form.xml');
+  const { xmlFormId } = await apiPostFile(`projects/${projectId}/forms`, 'test/s3-e2e/test-form.xml');
 
   log.info('Uploading attachments...');
   await Promise.all(
-    fs.readdirSync('test/s3-e2e/test-attachments')
-      .map(f => apiPostFile(`projects/{projectId}/forms/{xmlFormId}/draft/attachments/{filename}`, f)),
+    fs.readdirSync(attDir)
+      .filter(f => !f.startsWith('.'))
+      .map(f => apiPostFile(`projects/${projectId}/forms/${xmlFormId}/draft/attachments/${f}`, `${attDir}/${f}`)),
   );
 
-  log('Downloading attachments...');
-  const attachments = await apiGet(`projects/{projectId}/forms/{xmlFormId}/attachments`);
+  log('Downloading attachments list...');
+  const attachments = await apiGet(`projects/${projectId}/forms/${xmlFormId}/attachments`);
 
+  log.debug('Got attachments list:', attachments);
+  await allRedirect(attachments);
   for(const att of attachments) {
-    const res = await apiRawGet(`projects/{projectId}/forms/{xmlFormId}/attachments/${att}`);
+    const res = await apiRawHead(`projects/${projectId}/forms/${xmlFormId}/attachments/${att.name}`);
+    if(!(res instanceof Redirect) || res.status !== 307) {
+      throw new Error(`Unexpected redirect for attachment ${JSON.stringify(att)}: ${res}`);
+    }
 
-    // TODO assert some stuff about the returned attachment / headers
+    await assertFileEqualsFetch(att, res.location);
   }
+
+  log.report('All OK.');
+
+  function allRedirect(attachments) {
+    const TIMEOUT = 60_000; // ms
+    const timeout = Date.now() + TIMEOUT;
+
+    return new Promise((resolve, reject) => {
+      setImmediate(check);
+
+      async function check() {
+        for(const att of attachments) {
+          log.debug('allRedirect()', 'checking attachment:', att.name);
+          const res = await apiRawHead(`projects/${projectId}/forms/${xmlFormId}/attachments/${att.name}`);
+          if(!(res instanceof Redirect)) {
+            log.debug('allRedirect()', 'Attachment did not redirect:', att.name);
+            if(Date.now() > timeout) reject(new Error(`Timeout out after ${TIMEOUT/1000}s.`));
+            else {
+              log.debug('Sleeping...');
+              setTimeout(check, 500);
+            }
+            return;
+          }
+        }
+        resolve(); // all redirected
+      }
+    });
+  }
+}
+
+async function assertFileEqualsFetch({ name }, url) {
+  const filepath = `${attDir}/${name}`;
+  log.debug('assertFileEqualsFetch()', { filepath, url });
+
+  const res = await fetch(url);
+  should.ok(res.ok);
+
+  const expectedContentType = mimetypeFor(name);
+  const actualContentType = res.headers.get('content-type');
+  should.equal(actualContentType, expectedContentType);
+
+  const resContent = await res.buffer();
+  const fileContent = fs.readFileSync(filepath);
+  should.equal(resContent.length, fileContent.length);
+
+  // comparing streams would be faster; let's see how slow this is
+  for(let i=0; i<fileContent.length; ++i) {
+    should.equal(resContent[i], fileContent[i]);
+  }
+
+  log.info('File', name, 'matched content fetched from', url);
 }
 
 function apiPostFile(path, filePath) {
@@ -81,6 +155,19 @@ async function apiPost(path, body, headers) {
   return res.json();
 }
 
+function apiRawHead(path, headers) {
+  return apiFetch('HEAD', path, undefined, headers);
+}
+
+function apiRawGet(path, headers) {
+  return apiFetch('GET', path, undefined, headers);
+}
+
+async function apiGet(path, headers) {
+  const res = await apiFetch('GET', path, undefined, headers);
+  return res.json();
+}
+
 async function apiFetch(method, path, body, extraHeaders) {
   const url = `${serverUrl}/v1/${path}`;
 
@@ -94,9 +181,13 @@ async function apiFetch(method, path, body, extraHeaders) {
     method,
     body,
     headers,
+    redirect: 'manual',
   });
   log.debug(method, res.url, '->', res.status);
+
+  if(isRedirected(res)) return new Redirect(res);
   if(!res.ok) throw new Error(`${res.status}: ${await res.text()}`);
+
   return res;
 }
 
@@ -105,9 +196,15 @@ function base64(s) {
 }
 
 function mimetypeFor(f) {
+  // For more, see: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
   const extension = fileExtensionFrom(f);
   log.debug('fileExtensionFrom()', f, '->', extension);
   switch(extension) {
+    case 'bin' : return 'application/octet-stream';
+    case 'jpg' : return 'image/jpeg';
+    case 'png' : return 'image/png';
+    case 'svg' : return 'image/svg+xml';
+    case 'txt' : return 'text/plain';
     case 'xls' : return 'application/vnd.ms-excel';
     case 'xlsx': return 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
     case 'xml' : return 'application/xml';
@@ -121,4 +218,23 @@ function fileExtensionFrom(f) {
   } catch(err) {
     throw new Error(`Could not get file extension from filename '${f}'!`);
   }
+}
+
+function isRedirected(res) {
+  // should support res.redirected, but maybe old version
+  // See: https://www.npmjs.com/package/node-fetch#responseredirected
+  return res.redirected || (res.status >=300 && res.status < 400);
+}
+
+class Redirect {
+  constructor(res) {
+    this.props = Object.freeze({
+      status:   res.status,
+      location: res.headers.get('location'),
+      headers:  Object.freeze(res.headers.raw()),
+    });
+  }
+  get status()   { return this.props.status; }
+  get location() { return this.props.location; }
+  get headers()  { return this.props.headers; }
 }
