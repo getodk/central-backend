2dc72531c72dbb55c87cc0b024df2c551829db65
alxndrsn <alxndrsn>
2024-02-02 07:29:52 +0000

    disable eslint for test

--- test/s3-e2e/index.js
+++ test/s3-e2e/index.js
@@ -7,10 +7,10 @@
 // including this file, may be copied, modified, propagated, or distributed
 // except according to the terms contained in the LICENSE file.
 
+/* eslint-disable */
+
 const fs = require('node:fs');
 const fetch = require('node-fetch');
-const _ = require('lodash');
-const uuid = require('uuid').v4;
 const { randomBytes } = require('node:crypto');
 const { basename } = require('node:path');
 const { program } = require('commander');
