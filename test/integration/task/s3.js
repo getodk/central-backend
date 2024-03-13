59efaa5e483fcd7eb077ad7e562cf232ce91ae5b
alxndrsn <alxndrsn>
2024-03-13 11:49:03 +0000

    Add placeholder task tests

---
+++ test/integration/task/s3.js
@@ -0,0 +1,24 @@
+const should = require('should');
+const appRoot = require('app-root-path');
+const { testTask } = require('../setup');
+const { uploadPending } = require(appRoot + '/lib/task/s3');
+
+const TODO = () => { throw new Error('Needs implementation'); };
+
+describe('task: s3', () => {
+  describe('uploadPending()', () => {
+    it('should have loaded uploadPending()', () => (typeof uploadPending === 'function') || should.fail()); // FIXME remove this test
+
+    it('should not do anything if nothing to upload', testTask(() => {
+      TODO();
+    }));
+
+    it('should uploading pending blobs, and ignore others', testTask(() => {
+      TODO();
+    }));
+
+    it('should return error if uploading fails', testTask(() => {
+      TODO();
+    }));
+  });
+});
