aab4fae5fb9da48d8f6b6be47e05cea5573d7a2a
alxndrsn <alxndrsn>
2024-03-13 15:17:24 +0000

    one test

--- test/integration/task/s3.js
+++ test/integration/task/s3.js
@@ -5,12 +5,19 @@ const { uploadPending } = require(appRoot + '/lib/task/s3');
 
 const TODO = () => { throw new Error('Needs implementation'); };
 
-describe('task: s3', () => {
+describe.only('task: s3', () => {
   describe('uploadPending()', () => {
     it('should have loaded uploadPending()', () => (typeof uploadPending === 'function') || should.fail()); // FIXME remove this test
 
-    it('should not do anything if nothing to upload', testTask(() => {
-      TODO();
+    it('should not do anything if nothing to upload', testTask(async (container) => {
+      // given
+      global.s3mock.enable(container);
+
+      // when
+      await uploadPending();
+
+      // then
+      Object.keys(global.s3mock.s3bucket).should.equal(0);
     }));
 
     it('should uploading pending blobs, and ignore others', testTask(() => {
