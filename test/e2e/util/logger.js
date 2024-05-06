ab6773b3a9fec7bac2559372e58a07ed599b0a66
alxndrsn <alxndrsn>
2024-05-06 17:25:24 +0000

    logger: include level in messages

--- test/e2e/util/logger.js
+++ test/e2e/util/logger.js
@@ -4,7 +4,7 @@ const logLevel = process.env.LOG_LEVEL || 'INFO';
 module.exports = suiteName => {
   const _log = (level, ...args) => {
     if (LOG_LEVELS.indexOf(logLevel) > LOG_LEVELS.indexOf(level)) return;
-    console.log(`[${new Date().toISOString()}]`, `[${suiteName}]`, ...args);
+    console.log(`[${new Date().toISOString()}]`, level, `[${suiteName}]`, ...args);
   };
   const log  = (...args) => _log('INFO',   ...args);
   log.debug  = (...args) => _log('DEBUG',  ...args);
