801fb4c1faa21ffb690b0c424066470719e69f74
alxndrsn <alxndrsn>
2024-06-10 11:37:12 +0000

    CLI: add disabled handling; fix setFailedToPending() ref

--- test/integration/task/s3.js
+++ test/integration/task/s3.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const should = require('should');
 const appRoot = require('app-root-path');
 const { testTask } = require('../setup');
-const { uploadPending } = require(appRoot + '/lib/task/s3');
+const { getCount, setFailedToPending, uploadPending } = require(appRoot + '/lib/task/s3');
 const { Blob } = require(appRoot + '/lib/model/frames');
 
 // eslint-disable-next-line camelcase
@@ -12,92 +12,106 @@ const aBlobExistsWith = async (Blobs, { status: s3_status }) => {
 };
 
 describe('task: s3', () => {
-  const assertUploadCount = (expected) => {
-    global.s3.uploads.successful.should.equal(expected);
-  };
+  describe('s3 disabled', () => {
+    it('uploadPending() should fail', () => {
+      (() => uploadPending()).should.throw('S3 blob support is not enabled.');
+    });
 
-  describe('uploadPending()', () => {
-    it('should have loaded uploadPending()', () => (typeof uploadPending === 'function') || should.fail()); // FIXME remove this test
+    it('setFailedToPending() should fail', () => {
+      (() => setFailedToPending()).should.throw('S3 blob support is not enabled.');
+    });
 
-    it('should not do anything if nothing to upload', testTask(async () => {
-      // given
-      global.s3.enableMock();
+    it('getCount() should fail', () => {
+      (() => getCount()).should.throw('S3 blob support is not enabled.');
+    });
+  });
 
-      // when
-      await uploadPending(true);
+  describe('s3 enabled', () => {
+    const assertUploadCount = (expected) => {
+      global.s3.uploads.successful.should.equal(expected);
+    };
 
-      // then
-      assertUploadCount(0);
-    }));
+    describe('uploadPending()', () => {
+      it('should not do anything if nothing to upload', testTask(async () => {
+        // given
+        global.s3.enableMock();
 
-    it('should uploading pending blobs, and ignore others', testTask(async ({ Blobs }) => {
-      // given
-      global.s3.enableMock();
-      // and
-      await aBlobExistsWith(Blobs, { status: 'pending' });
-      await aBlobExistsWith(Blobs, { status: 'uploaded' });
-      await aBlobExistsWith(Blobs, { status: 'failed' });
-      await aBlobExistsWith(Blobs, { status: 'pending' });
-      await aBlobExistsWith(Blobs, { status: 'uploaded' });
-      await aBlobExistsWith(Blobs, { status: 'failed' });
-
-      // when
-      await uploadPending(true);
-
-      // then
-      assertUploadCount(2);
-    }));
-
-    it('should return error if uploading fails', testTask(async ({ Blobs }) => {
-      // given
-      global.s3.enableMock();
-      global.s3.error.onUpload = true;
-      // and
-      await aBlobExistsWith(Blobs, { status: 'pending' });
-
-      // when
-      try {
+        // when
         await uploadPending(true);
-        should.fail('should have thrown');
-      } catch (err) {
+
         // then
-        err.message.should.equal('Mock error when trying to upload blobs.');
-      }
+        assertUploadCount(0);
+      }));
 
-      // and
-      assertUploadCount(0);
-    }));
+      it('should uploading pending blobs, and ignore others', testTask(async ({ Blobs }) => {
+        // given
+        global.s3.enableMock();
+        // and
+        await aBlobExistsWith(Blobs, { status: 'pending' });
+        await aBlobExistsWith(Blobs, { status: 'uploaded' });
+        await aBlobExistsWith(Blobs, { status: 'failed' });
+        await aBlobExistsWith(Blobs, { status: 'pending' });
+        await aBlobExistsWith(Blobs, { status: 'uploaded' });
+        await aBlobExistsWith(Blobs, { status: 'failed' });
 
-    it('should not allow failure to affect previous or future uploads', testTask(async ({ Blobs }) => {
-      // given
-      global.s3.enableMock();
-      global.s3.error.onUpload = 3;
-      // and
-      await aBlobExistsWith(Blobs, { status: 'pending' });
-      await aBlobExistsWith(Blobs, { status: 'pending' });
-      await aBlobExistsWith(Blobs, { status: 'pending' });
-
-      // when
-      try {
+        // when
         await uploadPending(true);
-        should.fail('should have thrown');
-      } catch (err) {
+
         // then
-        err.message.should.equal('Mock error when trying to upload #3');
-      }
+        assertUploadCount(2);
+      }));
 
-      // and
-      assertUploadCount(2);
+      it('should return error if uploading fails', testTask(async ({ Blobs }) => {
+        // given
+        global.s3.enableMock();
+        global.s3.error.onUpload = true;
+        // and
+        await aBlobExistsWith(Blobs, { status: 'pending' });
+
+        // when
+        try {
+          await uploadPending(true);
+          should.fail('should have thrown');
+        } catch (err) {
+          // then
+          err.message.should.equal('Mock error when trying to upload blobs.');
+        }
+
+        // and
+        assertUploadCount(0);
+      }));
+
+      it('should not allow failure to affect previous or future uploads', testTask(async ({ Blobs }) => {
+        // given
+        global.s3.enableMock();
+        global.s3.error.onUpload = 3;
+        // and
+        await aBlobExistsWith(Blobs, { status: 'pending' });
+        await aBlobExistsWith(Blobs, { status: 'pending' });
+        await aBlobExistsWith(Blobs, { status: 'pending' });
+
+        // when
+        try {
+          await uploadPending(true);
+          should.fail('should have thrown');
+        } catch (err) {
+          // then
+          err.message.should.equal('Mock error when trying to upload #3');
+        }
+
+        // and
+        assertUploadCount(2);
 
 
-      // given
-      await aBlobExistsWith(Blobs, { status: 'pending' });
+        // given
+        await aBlobExistsWith(Blobs, { status: 'pending' });
 
-      // when
-      await uploadPending(true);
+        // when
+        await uploadPending(true);
 
-      // then
-      assertUploadCount(3);
-    }));
+        // then
+        assertUploadCount(3);
+      }));
+    });
   });
 });
