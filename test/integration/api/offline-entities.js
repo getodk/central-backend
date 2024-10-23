c65ea25f880c7d0c20f0f3e7da1ce37912557f8f
Kathleen Tuite <ktuite@getodk.org>
2024-10-23 15:13:32 -0700

    entity submission create-as-update

--- test/integration/api/offline-entities.js
+++ test/integration/api/offline-entities.js
@@ -936,7 +936,7 @@ describe('Offline Entities', () => {
       backlogCount.should.equal(0);
     }));
 
-    it('should apply an entity update as a create', testOfflineEntities(async (service, container) => {
+    it('should apply an entity update as a create [update-as-create]', testOfflineEntities(async (service, container) => {
       const asAlice = await service.login('alice');
       const branchId = uuid();
       const newUuid = uuid();
@@ -1003,7 +1003,7 @@ describe('Offline Entities', () => {
       backlogCount.should.equal(0);
     }));
 
-    it('should apply an entity update as a create followed by another update', testOfflineEntities(async (service, container) => {
+    it('should apply an entity update as a create followed by another update [update-as-create]', testOfflineEntities(async (service, container) => {
       const asAlice = await service.login('alice');
       const branchId = uuid();
       const newUuid = uuid();
@@ -1091,7 +1091,7 @@ describe('Offline Entities', () => {
       backlogCount.should.equal(0);
     }));
 
-    it.skip('should apply an entity update as a create, and then properly handle the delayed create', testOfflineEntities(async (service, container) => {
+    it('should apply an entity update as a create, and then properly handle the delayed create [create-as-update]', testOfflineEntities(async (service, container) => {
       const asAlice = await service.login('alice');
       const branchId = uuid();
 
@@ -1126,8 +1126,8 @@ describe('Offline Entities', () => {
           body.currentVersion.data.should.eql({ status: 'checked in' });
           body.currentVersion.label.should.eql('auto generated');
           body.currentVersion.branchId.should.equal(branchId);
+          body.currentVersion.branchBaseVersion.should.equal(1);
           should.not.exist(body.currentVersion.baseVersion);
-          should.not.exist(body.currentVersion.branchBaseVersion); // No base version because this is a create, though maybe this should be here.
           should.not.exist(body.currentVersion.trunkVersion);
         });
 
@@ -1136,25 +1136,16 @@ describe('Offline Entities', () => {
 
       // First submission creates the entity, but this will be processed as an update
       await asAlice.post('/v1/projects/1/forms/offlineEntity/submissions')
-        .send(testData.instances.offlineEntity.two
-          .replace('branchId=""', `branchId="${branchId}"`)
-        )
+        .send(testData.instances.offlineEntity.two)
         .set('Content-Type', 'application/xml')
         .expect(200);
 
       await exhaust(container);
 
-      // In the default behavior, attempting create on an entity that already exists causes a conflict error.
-      await asAlice.get('/v1/projects/1/forms/offlineEntity/submissions/two/audits')
-        .expect(200)
-        .then(({ body }) => {
-          body[0].details.errorMessage.should.eql('A resource already exists with uuid value(s) of 12345678-1234-4123-8234-123456789ddd.');
-        });
-
       await asAlice.get(`/v1/projects/1/datasets/people/entities/12345678-1234-4123-8234-123456789ddd`)
         .expect(200)
         .then(({ body }) => {
-          body.currentVersion.version.should.equal(1);
+          body.currentVersion.version.should.equal(2);
         });
     }));
 
@@ -1431,6 +1422,42 @@ describe('Offline Entities', () => {
           body[0].source.forceProcessed.should.equal(true);
           body[0].source.updateAsCreate.should.equal(true);
           body[0].source.upsert.should.equal(true);
+        });
+    }));
+
+    it('should mark a create-as-update', testOfflineEntities(async (service, container) => {
+      const asAlice = await service.login('alice');
+      const branchId = uuid();
+
+      // Send update first
+      await asAlice.post('/v1/projects/1/forms/offlineEntity/submissions')
+        .send(testData.instances.offlineEntity.two
+          .replace('create="1"', 'update="1"')
+          .replace('branchId=""', `branchId="${branchId}"`)
+          .replace('two', 'two-update')
+          .replace('baseVersion=""', 'baseVersion="1"')
+        )
+        .set('Content-Type', 'application/xml')
+        .expect(200);
+
+      await exhaust(container);
+
+      // Force the update submission to be processed as a create
+      await container.Entities.processBacklog(true);
+
+      // Send create in next to be applied as an update
+      await asAlice.post('/v1/projects/1/forms/offlineEntity/submissions')
+        .send(testData.instances.offlineEntity.two)
+        .set('Content-Type', 'application/xml')
+        .expect(200);
+
+      await exhaust(container);
+
+      await asAlice.get(`/v1/projects/1/datasets/people/entities/12345678-1234-4123-8234-123456789ddd/versions`)
+        .expect(200)
+        .then(({ body }) => {
+          body[1].source.createAsUpdate.should.equal(true);
+          body[1].source.forceProcessed.should.equal(false);
         });
     }));
 
