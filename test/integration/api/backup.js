8c648e1d1fb195d571e658232e15a831f24b4db1
alxndrsn <alxndrsn>
2024-12-10 06:20:45 +0000

    fix?

--- test/integration/api/backup.js
+++ test/integration/api/backup.js
@@ -5,6 +5,7 @@ const { httpZipResponseToFiles } = require('../../util/zip');
 describe('api: /backup', () => {
   describe('POST', function () {
     this.timeout(10000);
+
     it('should reject if the user cannot backup', testService((service) =>
       service.login('chelsea', (asChelsea) =>
         asChelsea.post('/v1/backup').expect(403))));
@@ -12,16 +13,16 @@ describe('api: /backup', () => {
     it('should return a valid zip file if the user can backup @slow', testService((service) =>
       service.login('alice', (asAlice) =>
         httpZipResponseToFiles(asAlice.post('/v1/backup').expect(200))
-          .then(({ filenames, ...files }) => {
+          .then(({ filenames, files }) => {
             const constantFiles = ['keepalive', 'keys.json', 'toc.dat'];
-            files.should.have.properties(constantFiles);
+            [ ...files.keys() ].should.eqlInAnyOrder(constantFiles);
 
             const datFiles = without(constantFiles, filenames);
             // Because /backup ALWAYS uses config('default.database'), this list
             // may be empty in some environments, including CI.
             datFiles.should.matchEvery(/\.dat\.gz$/);
 
-            const keysJson = JSON.parse(files['keys.json']);
+            const keysJson = JSON.parse(files.get('keys.json'));
             keysJson.should.only.have.keys('iv', 'local', 'privkey', 'pubkey', 'salt');
             keysJson.iv.should.be.a.String();
             keysJson.privkey.should.be.a.String();
