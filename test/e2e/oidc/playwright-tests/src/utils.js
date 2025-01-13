31652a6e8bc513faa93af424414507d74756f62f
alxndrsn <alxndrsn>
2025-01-13 14:21:44 +0000

    test/e2e/oidc: fix webkit race condition
    This seems to solve intermittent test failures in webkit, where the password was being entered into the username form field.

--- test/e2e/oidc/playwright-tests/src/utils.js
+++ test/e2e/oidc/playwright-tests/src/utils.js
@@ -69,9 +69,16 @@ function assertTitle(page, expectedTitle) {
 }
 
 async function fillLoginForm(page, { username, password }) {
+  // Wait for autofocus.  On webkit, it looks like `autofocus` on the username
+  // (`login`) field can sometimes steal focus after the `password` field
+  // locator has been successfully executed.
+  await sleep(1);
+
   await page.locator('input[name=login]').fill('playwright-' + username);
   await page.locator('input[name=password]').fill(password);
+
   await page.locator('button[type=submit]').click();
+
   await page.getByRole('button', { name: 'Continue' }).click();
 }
 
@@ -79,5 +86,11 @@ function initTest({ browserName, page }, testInfo) {
   page.on('console', msg => {
     const level = msg.type().toUpperCase();
     console.log(level, `[${browserName}:${testInfo.title}]`, msg.text());
+  });
+}
+
+async function sleep(seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
   });
 }
