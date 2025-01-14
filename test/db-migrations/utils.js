9b76566f72e005a0eb8be8c9febfaf9c46f309d6
alxndrsn <alxndrsn>
2025-01-14 07:01:37 +0000

    revert renaming

--- test/db-migrations/utils.js
+++ test/db-migrations/utils.js
@@ -77,22 +77,22 @@ function assertRowsMatch(actualRows, expectedRows) {
 
   const remainingRows = [...actualRows];
   for (let i=0; i<expectedRows.length; ++i) { // eslint-disable-line no-plusplus
-    const expectedRow = expectedRows[i];
+    const x = expectedRows[i];
     let found = false;
     for (let j=0; j<remainingRows.length; ++j) { // eslint-disable-line no-plusplus
       const rr = remainingRows[j];
       try {
-        assertIncludes(rr, expectedRow); // eslint-disable-line no-use-before-define
+        assertIncludes(rr, x); // eslint-disable-line no-use-before-define
         remainingRows.splice(j, 1);
         found = true;
         break;
       } catch (err) { /* keep searching */ }
     }
     if (!found) {
-      const filteredRemainingRows = remainingRows.map(r => _.pick(r, Object.keys(expectedRow)));
+      const filteredRemainingRows = remainingRows.map(r => _.pick(r, Object.keys(x)));
       assert.fail(
         `Expected row ${i} not found:\njson=` +
-        JSON.stringify({ remainingRows, filteredRemainingRows, expectedRow }),
+        JSON.stringify({ remainingRows, filteredRemainingRows, expectedRow: x }),
       );
     }
   }
