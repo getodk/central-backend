7d85dcff6c17872916833db592667386e18d0894
alxndrsn <alxndrsn>
2025-02-04 03:35:00 +0000

    remove contentType from test which shouldn't have it

--- test/db-migrations/20250113-01-disable-nullable-blob-content-types.spec.js
+++ test/db-migrations/20250113-01-disable-nullable-blob-content-types.spec.js
@@ -36,8 +36,8 @@ describeMigration('20250113-01-disable-nullable-blob-content-types', ({ runMigra
     const { md5, sha } = aBlob();
 
     const created = await db.oneFirst(sql`
-      INSERT INTO blobs (md5, sha, "contentType")
-        VALUES(${md5}, ${sha}, DEFAULT)
+      INSERT INTO blobs (md5, sha)
+        VALUES(${md5}, ${sha})
         RETURNING "contentType"
     `);
 
