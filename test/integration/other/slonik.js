0dcdaa4c7d6790c9959242c28311834598f549b8
alxndrsn <alxndrsn>
2025-03-12 11:50:55 +0000

    all

--- test/integration/other/slonik.js
+++ test/integration/other/slonik.js
@@ -4,13 +4,13 @@ const { testContainer } = require('../setup');
 
 describe('slonik', () => {
   describe('query()', () => {
-    it('should accept symbols', testContainer(async ({ db }) => {
+    it('should accept symbols', testContainer(async ({ all }) => {
       const { SqlToken } = require('slonik/dist/src/tokens');
 
       SqlToken.should.be.a.Symbol();
       SqlToken.description.should.equal('SLONIK_TOKEN_SQL');
 
-      const res = await db.all(sql`
+      const res = await all(sql`
         SELECT * FROM roles WHERE id=${{ type: SqlToken, values: [], sql: '1 OR TRUE' }}
       `);
 
@@ -19,11 +19,11 @@ describe('slonik', () => {
       ]);
     }));
 
-    it('should not accept non-symbols', testContainer(async ({ db }) => {
+    it('should not accept non-symbols', testService(async ({ all }) => {
       let caught;
 
       try {
-        await db.all(sql`
+        await all(sql`
           SELECT * FROM roles WHERE id=${{ type: 'SLONIK_TOKEN_SQL', values: [], sql: '1 OR TRUE' }}
         `).should.be.rejected();
       } catch (err) {
