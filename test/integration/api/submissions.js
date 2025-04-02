c771d39f22da272a0e48a337aa1eb522821b5622
alxndrsn <alxndrsn>
2025-04-02 06:54:56 +0000

    Add error details

--- test/integration/api/submissions.js
+++ test/integration/api/submissions.js
@@ -63,6 +63,7 @@ describe('api: /submission', () => {
             body.should.eql({
               code: 400.39,
               message: 'Multipart form content failed to parse.',
+              details: 'Multipart: Boundary not found',
             });
           }))));
 
@@ -77,6 +78,7 @@ describe('api: /submission', () => {
             body.should.eql({
               code: 400.39,
               message: 'Multipart form content failed to parse.',
+              details: 'Unexpected end of form',
             });
           }))));
 
@@ -91,6 +93,7 @@ describe('api: /submission', () => {
             body.should.eql({
               code: 400.39,
               message: 'Multipart form content failed to parse.',
+              details: 'Malformed part header',
             });
           }))));
 
