c21187a017d1e59f1bc4a432d576447a963a168d
alxndrsn <alxndrsn>
2025-05-02 14:18:31 +0000

    nicer esxcaping

--- test/db-partial-migrations/test.sh
+++ test/db-partial-migrations/test.sh
@@ -93,10 +93,10 @@ log "Checking final database schema..."
 if ! diff \
     test/db-partial-migrations/expected-schema.sql \
     <(
-      pg_dump --schema-only "$(node -e "
-        const { host, database, user, password } = require('config').get('default.database');
-        console.log(\`postgres://\${user}:\${password}@\${host}/\${database}\`);
-      ")"
+      pg_dump --schema-only "$(node -e '
+        const { host, database, user, password } = require("config").get("default.database");
+        console.log(\`postgres://${user}:${password}@${host}/${database}\`);
+      ')"
     ); then
   log "!!!"
   log "!!! Schema differences detected.  See above for details."
