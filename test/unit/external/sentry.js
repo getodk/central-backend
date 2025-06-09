5b052ef9715e90c8ca0dfe901340b6cf5f665715
alxndrsn <alxndrsn>
2025-06-09 10:16:48 +0000

    lint; test cases with double-digit project

--- test/unit/external/sentry.js
+++ test/unit/external/sentry.js
@@ -1,47 +1,48 @@
 const appRoot = require('app-root-path');
 const { filterXmlFormIdFromUrl } = require(appRoot + '/lib/external/sentry');
 
-describe.only('external/sentry', () => {
+describe('external/sentry', () => {
+  /* eslint-disable no-multi-spaces */
   describe('filterXmlFormIdFromUrl()', () => {
     [
       [ '/forms/:id/restore', '/forms/1/restore',  '/forms/1/restore' ],
       [ '/forms/:id/restore', '/forms/11/restore', '/forms/11/restore' ],
 
       [ '/projects/:projectId/forms/:xmlFormId', '/projects/1/forms/1',  '/projects/1/forms/:xmlFormId' ],
-      [ '/projects/:projectId/forms/:xmlFormId', '/projects/1/forms/1',  '/projects/1/forms/:xmlFormId' ],
+      [ '/projects/:projectId/forms/:xmlFormId', '/projects/11/forms/1', '/projects/11/forms/:xmlFormId' ],
 
       [ '/projects/:projectId/forms/:xmlFormId', '/projects/1/forms/11',  '/projects/1/forms/:xmlFormId' ],
-      [ '/projects/:projectId/forms/:xmlFormId', '/projects/1/forms/11',  '/projects/1/forms/:xmlFormId' ],
+      [ '/projects/:projectId/forms/:xmlFormId', '/projects/11/forms/11', '/projects/11/forms/:xmlFormId' ],
 
       [ '/projects/:projectId/forms/:xmlFormId', '/projects/1/forms/form_name',  '/projects/1/forms/:xmlFormId' ],
-      [ '/projects/:projectId/forms/:xmlFormId', '/projects/1/forms/form_name',  '/projects/1/forms/:xmlFormId' ],
+      [ '/projects/:projectId/forms/:xmlFormId', '/projects/11/forms/form_name', '/projects/11/forms/:xmlFormId' ],
 
       [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/1/forms/1.svc',  '/projects/1/forms/:xmlFormId.svc' ],
-      [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/1/forms/1.svc',  '/projects/1/forms/:xmlFormId.svc' ],
+      [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/11/forms/1.svc', '/projects/11/forms/:xmlFormId.svc' ],
 
       [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/1/forms/11.svc',  '/projects/1/forms/:xmlFormId.svc' ],
-      [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/1/forms/11.svc',  '/projects/1/forms/:xmlFormId.svc' ],
+      [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/11/forms/11.svc', '/projects/11/forms/:xmlFormId.svc' ],
 
       [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/1/forms/form_name.svc',  '/projects/1/forms/:xmlFormId.svc' ],
-      [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/1/forms/form_name.svc',  '/projects/1/forms/:xmlFormId.svc' ],
+      [ '/projects/:projectId/forms/:xmlFormId.svc', '/projects/11/forms/form_name.svc', '/projects/11/forms/:xmlFormId.svc' ],
 
       [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/1/forms/1/more-path',  '/projects/1/forms/:xmlFormId/more-path' ],
-      [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/1/forms/1/more-path',  '/projects/1/forms/:xmlFormId/more-path' ],
+      [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/11/forms/1/more-path', '/projects/11/forms/:xmlFormId/more-path' ],
 
       [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/1/forms/11/more-path',  '/projects/1/forms/:xmlFormId/more-path' ],
-      [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/1/forms/11/more-path',  '/projects/1/forms/:xmlFormId/more-path' ],
+      [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/11/forms/11/more-path', '/projects/11/forms/:xmlFormId/more-path' ],
 
       [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/1/forms/form_name/more-path',  '/projects/1/forms/:xmlFormId/more-path' ],
-      [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/1/forms/form_name/more-path',  '/projects/1/forms/:xmlFormId/more-path' ],
+      [ '/projects/:projectId/forms/:xmlFormId/more-path', '/projects/11/forms/form_name/more-path', '/projects/11/forms/:xmlFormId/more-path' ],
 
       [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/1/forms/1.svc/more-path',  '/projects/1/forms/:xmlFormId.svc/more-path' ],
-      [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/1/forms/1.svc/more-path',  '/projects/1/forms/:xmlFormId.svc/more-path' ],
+      [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/11/forms/1.svc/more-path', '/projects/11/forms/:xmlFormId.svc/more-path' ],
 
       [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/1/forms/11.svc/more-path',  '/projects/1/forms/:xmlFormId.svc/more-path' ],
-      [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/1/forms/11.svc/more-path',  '/projects/1/forms/:xmlFormId.svc/more-path' ],
+      [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/11/forms/11.svc/more-path', '/projects/11/forms/:xmlFormId.svc/more-path' ],
 
       [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/1/forms/form_name.svc/more-path',  '/projects/1/forms/:xmlFormId.svc/more-path' ],
-      [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/1/forms/form_name.svc/more-path',  '/projects/1/forms/:xmlFormId.svc/more-path' ],
+      [ '/projects/:projectId/forms/:xmlFormId.svc/more-path', '/projects/11/forms/form_name.svc/more-path', '/projects/11/forms/:xmlFormId.svc/more-path' ],
     ].forEach(([ transaction, originalUrl, expectedUrl ]) => {
       it(`should convert ${originalUrl} to ${expectedUrl}`, () => {
         filterXmlFormIdFromUrl(transaction, originalUrl).should.eql(expectedUrl);
