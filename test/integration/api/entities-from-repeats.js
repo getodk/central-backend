f3f72c090f2ddefdc149a1175d6405a850a978bd
Kathleen Tuite <ktuite@getodk.org>
2025-07-18 13:42:12 -0700

    Detect multiple datasets in a form and assign properties to each one

--- test/integration/api/entities-from-repeats.js
+++ test/integration/api/entities-from-repeats.js
@@ -45,4 +45,189 @@ describe('Entities from Repeats', () => {
         .expect(200);
     }));
   });
+
+  describe('assigning properties to the individual datasets', () => {
+    it('should assign properties to dataset trees in a repeat group', testService(async (service) => {
+      const asAlice = await service.login('alice');
+
+      await asAlice.post('/v1/projects/1/forms?publish=true')
+        .send(testData.forms.repeatEntityTrees)
+        .set('Content-Type', 'application/xml')
+        .expect(200);
+
+      await asAlice.get('/v1/projects/1/datasets/trees')
+        .then(({ body }) => {
+          const { createdAt, linkedForms, properties, sourceForms, lastUpdate, ...ds } = body;
+
+          ds.should.be.eql({
+            name: 'trees',
+            projectId: 1,
+            approvalRequired: false,
+            ownerOnly: false
+          });
+
+          createdAt.should.not.be.null();
+
+          lastUpdate.should.be.isoDate();
+
+          linkedForms.should.be.eql([]);
+
+          sourceForms.should.be.eql([
+            { name: 'Repeat Trees', xmlFormId: 'repeatEntityTrees' },
+          ]);
+
+          properties.map(({ publishedAt, ...p }) => {
+            publishedAt.should.be.isoDate();
+            return p;
+          }).should.be.eql([
+            { name: 'species', odataName: 'species', forms: [ { name: 'Repeat Trees', xmlFormId: 'repeatEntityTrees' } ] },
+            { name: 'circumference', odataName: 'circumference', forms: [ { name: 'Repeat Trees', xmlFormId: 'repeatEntityTrees' } ] },
+          ]);
+
+        });
+    }));
+
+    it('should assign properties to households and people datasets at parent and child repeat levels', testService(async (service) => {
+      const asAlice = await service.login('alice');
+
+      await asAlice.post('/v1/projects/1/forms?publish=true')
+        .send(testData.forms.repeatEntityHousehold)
+        .set('Content-Type', 'application/xml')
+        .expect(200);
+
+      await asAlice.get('/v1/projects/1/datasets/households')
+        .then(({ body }) => {
+          const { createdAt, linkedForms, properties, sourceForms, lastUpdate, ...ds } = body;
+
+          ds.should.be.eql({
+            name: 'households',
+            projectId: 1,
+            approvalRequired: false,
+            ownerOnly: false
+          });
+
+          createdAt.should.not.be.null();
+
+          lastUpdate.should.be.isoDate();
+
+          linkedForms.should.be.eql([]);
+
+          sourceForms.should.be.eql([
+            { name: 'Household and people', xmlFormId: 'repeatEntityHousehold' },
+          ]);
+
+          properties.map(({ publishedAt, ...p }) => {
+            publishedAt.should.be.isoDate();
+            return p;
+          }).should.be.eql([
+            { name: 'hh_id', odataName: 'hh_id', forms: [ { name: 'Household and people', xmlFormId: 'repeatEntityHousehold' } ] },
+            { name: 'count', odataName: 'count', forms: [ { name: 'Household and people', xmlFormId: 'repeatEntityHousehold' } ] },
+          ]);
+
+        });
+
+      await asAlice.get('/v1/projects/1/datasets/people')
+        .then(({ body }) => {
+          const { createdAt, linkedForms, properties, sourceForms, lastUpdate, ...ds } = body;
+
+          ds.should.be.eql({
+            name: 'people',
+            projectId: 1,
+            approvalRequired: false,
+            ownerOnly: false
+          });
+
+          createdAt.should.not.be.null();
+
+          lastUpdate.should.be.isoDate();
+
+          linkedForms.should.be.eql([]);
+
+          sourceForms.should.be.eql([
+            { name: 'Household and people', xmlFormId: 'repeatEntityHousehold' },
+          ]);
+
+          properties.map(({ publishedAt, ...p }) => {
+            publishedAt.should.be.isoDate();
+            return p;
+          }).should.be.eql([
+            { name: 'full_name', odataName: 'full_name', forms: [ { name: 'Household and people', xmlFormId: 'repeatEntityHousehold' } ] },
+            { name: 'age', odataName: 'age', forms: [ { name: 'Household and people', xmlFormId: 'repeatEntityHousehold' } ] },
+          ]);
+
+        });
+    }));
+
+    it('should assign properties to farm and farmer datasets at different levels', testService(async (service) => {
+      const asAlice = await service.login('alice');
+
+      await asAlice.post('/v1/projects/1/forms?publish=true')
+        .send(testData.forms.multiEntityFarm)
+        .set('Content-Type', 'application/xml')
+        .expect(200);
+
+      await asAlice.get('/v1/projects/1/datasets/farms')
+        .then(({ body }) => {
+          const { createdAt, linkedForms, properties, sourceForms, lastUpdate, ...ds } = body;
+
+          ds.should.be.eql({
+            name: 'farms',
+            projectId: 1,
+            approvalRequired: false,
+            ownerOnly: false
+          });
+
+          createdAt.should.not.be.null();
+
+          lastUpdate.should.be.isoDate();
+
+          linkedForms.should.be.eql([]);
+
+          sourceForms.should.be.eql([
+            { name: 'Farms and Farmers - Multi Level Entities', xmlFormId: 'multiEntityFarm' },
+          ]);
+
+          properties.map(({ publishedAt, ...p }) => {
+            publishedAt.should.be.isoDate();
+            return p;
+          }).should.be.eql([
+            { name: 'farm_id', odataName: 'farm_id', forms: [ { name: 'Farms and Farmers - Multi Level Entities', xmlFormId: 'multiEntityFarm' } ] },
+            { name: 'geometry', odataName: 'geometry', forms: [ { name: 'Farms and Farmers - Multi Level Entities', xmlFormId: 'multiEntityFarm' } ] },
+            { name: 'acres', odataName: 'acres', forms: [ { name: 'Farms and Farmers - Multi Level Entities', xmlFormId: 'multiEntityFarm' } ] },
+          ]);
+
+        });
+
+      await asAlice.get('/v1/projects/1/datasets/farmers')
+        .then(({ body }) => {
+          const { createdAt, linkedForms, properties, sourceForms, lastUpdate, ...ds } = body;
+
+          ds.should.be.eql({
+            name: 'farmers',
+            projectId: 1,
+            approvalRequired: false,
+            ownerOnly: false
+          });
+
+          createdAt.should.not.be.null();
+
+          lastUpdate.should.be.isoDate();
+
+          linkedForms.should.be.eql([]);
+
+          sourceForms.should.be.eql([
+            { name: 'Farms and Farmers - Multi Level Entities', xmlFormId: 'multiEntityFarm' },
+          ]);
+
+          properties.map(({ publishedAt, ...p }) => {
+            publishedAt.should.be.isoDate();
+            return p;
+          }).should.be.eql([
+            { name: 'full_name', odataName: 'full_name', forms: [ { name: 'Farms and Farmers - Multi Level Entities', xmlFormId: 'multiEntityFarm' } ] },
+            { name: 'age', odataName: 'age', forms: [ { name: 'Farms and Farmers - Multi Level Entities', xmlFormId: 'multiEntityFarm' } ] },
+          ]);
+
+        });
+    }));
+  });
 });
