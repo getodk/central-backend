33def0ba0b60c55e692bd787fd1f33a1d671603a
alxndrsn <alxndrsn>
2025-08-07 05:41:09 +0000

    lint

--- test/e2e/actor-race/index.js
+++ test/e2e/actor-race/index.js
@@ -8,8 +8,6 @@
 // except according to the terms contained in the LICENSE file.
 
 const fs = require('node:fs');
-const _ = require('lodash');
-const uuid = require('uuid').v4;
 const { program } = require('commander');
 
 const SUITE_NAME = 'test/e2e/soak';
@@ -25,8 +23,6 @@ program
     ;
 program.parse();
 const { serverUrl, userEmail, userPassword, formPath, logDirectory } = program.opts();
-
-const submissionTemplate = fs.readFileSync('./250q-submission.template.xml', { encoding:'utf8' }).trim();
 
 log(`Using form: ${formPath}`);
 log(`Connecting to ${serverUrl} with user ${userEmail}...`);
@@ -107,7 +103,7 @@ async function soakTest() {
 }
 
 async function dbQuery(sqlQuery) {
-  const client = new (require('pg').Client)(require('../../../config/default').default.database);
+  const client = new (require('pg').Client)(require('../../../config/default.json').default.database);
   await client.connect();
 
   const { rows } = await client.query(sqlQuery);
@@ -125,8 +121,8 @@ async function dbCount(sqlQuery) {
   return +count;
 }
 
-async function countAssignedButDeletedActors() {
-  return await dbCount(`
+function countAssignedButDeletedActors() {
+  return dbCount(`
     SELECT COUNT(*) AS count
       FROM assignments
       JOIN actors AS actors ON actors.id=assignments."actorId"
@@ -134,156 +130,10 @@ async function countAssignedButDeletedActors() {
   `);
 }
 
-function doSoakTest(name, throughput, throughputPeriod, testDuration, minimumSuccessThreshold, fn) {
-  log.info('Starting soak test:', name);
-  log.info('        throughput:', throughput, 'per period');
-  log.info('  throughputPeriod:', throughputPeriod, 'ms');
-  log.info('      testDuration:', durationForHumans(testDuration));
-  log.info('-------------------------------');
-  return new Promise((resolve, reject) => {
-    try {
-      const successes = [];
-      const sizes = [];
-      const fails = [];
-      const results = [];
-      const sleepyTime = +throughputPeriod / +throughput;
-
-      let iterationCount = 0;
-      let completedIterations = 0;
-      const iterate = async () => {
-        const n = iterationCount++;
-        const started = Date.now();
-        try {
-          const size = await fn(n);
-          const finished = Date.now();
-          const time = finished - started;
-          successes.push(time);
-          sizes.push(size);
-          results[n] = { success:true,  started, finished, time, size };
-        } catch(err) {
-          fails.push(err.message);
-          results[n] = { success:false, started, finished:Date.now(), err:{ message:err.message, stack:err.stack } };
-        } finally {
-          ++completedIterations;
-        }
-      };
-
-      iterate();
-      const timerId = setInterval(iterate, sleepyTime);
-
-      setTimeout(async () => {
-        clearTimeout(timerId);
-
-        const maxDrainDuration = 120_000;
-        await new Promise(resolve => {
-          log.info(`Waiting up to ${durationForHumans(maxDrainDuration)} for test drainage...`);
-          const maxDrainTimeout = Date.now() + maxDrainDuration;
-          const drainPulse = 500;
-
-          checkDrain();
-
-          function checkDrain() {
-            log.debug('Checking drain status...');
-            if(Date.now() > maxDrainTimeout) {
-              log.info('Drain timeout exceeded.');
-              return resolve();
-            } else if(completedIterations >= iterationCount) {
-              log.info('All connections have completed.');
-              return resolve();
-            }
-            log.debug(`Drainage not complete.  Still Waiting for ${iterationCount - results.length} connections.  Sleeping for ${durationForHumans(drainPulse)}...`);
-            setTimeout(checkDrain, drainPulse);
-          }
-        });
-
-        fs.writeFileSync(`${logPath}/${name}.extras.log.json`, JSON.stringify(results, null, 2));
-
-        const successPercent = 100 * successes.length / iterationCount;
-
-        log.report('--------------------------');
-        log.report('              Test:', name);
-        log.report('     Test duration:', testDuration);
-        log.report('    Total requests:', iterationCount);
-        log.report('Success % required:', `${minimumSuccessThreshold}%`);
-        log.report('         Successes:', successes.length, `(${Math.floor(successPercent)}%)`);
-        log.report('        Throughput:', oneDp((1000 * successes.length / testDuration)), 'reqs/s');
-        log.report('          Failures:', fails.length);
-        log.report('    Response times:');
-        log.report('              mean:', durationForHumans(_.mean(successes)));
-        log.report('               min:', _.min(successes), 'ms');
-        log.report('               max:', _.max(successes), 'ms');
-        log.report('    Response sizes:');
-        log.report('               min:', _.min(sizes), 'b');
-        log.report('               max:', _.max(sizes), 'b');
-        if(fails.length) log.report('            Errors:');
-        [ ...new Set(fails) ].map(m => log.report(`              * ${m.replace(/\n/g, '\\n')}`));
-        log.report('--------------------------');
-
-        if(_.min(sizes) !== _.max(sizes)) reportFatalError('VARIATION IN RESPONSE SIZES MAY INDICATE SERIOUS ERRORS SERVER-SIDE');
-
-        if(successPercent < minimumSuccessThreshold) reportFatalError('MINIMUM SUCCESS THRESHOLD WAS NOT MET');
-
-        if(fails.length) reportWarning('REQUEST FAILURES MAY AFFECT SUBSEQUENT SOAK TESTS');
-
-        resolve();
-      }, +testDuration);
-    } catch(err) {
-      reject(err);
-    }
-  });
-}
-
-function reportFatalError(message) {
-  reportWarning(message);
-  process.exit(1);
-}
-
-function reportWarning(message) {
-  log.report('!!!');
-  log.report('!!!');
-  log.report(`!!! ${message}!`);
-  log.report('!!!');
-  log.report('!!!');
-  log.report('--------------------------');
-}
-
-function randomSubmission(n, projectId, formId) {
-  const headers = {
-    'Content-Type': 'multipart/form-data; boundary=foo',
-    'X-OpenRosa-Version': '1.0',
-  };
-
-  const body = `--foo\r
-Content-Disposition: form-data; name="xml_submission_file"; filename="submission.xml"\r
-Content-Type: application/xml\r
-\r
-${submissionTemplate
-  .replace(/{{uuid}}/g, () => uuid())
-  .replace(/{{randInt}}/g, randInt)
-}
-\r
---foo--`;
-
-  return api.apiPostAndDump('randomSubmission', n, `projects/${projectId}/forms/${formId}/submissions`, body, headers);
-}
-
 function randInt(max=9999) {
   return Math.floor(Math.random() * max);
 }
 
-function exportZipWithDataAndMedia(n, projectId, formId) {
-  return api.apiGetToFile('exportZipWithDataAndMedia', n, `projects/${projectId}/forms/${formId}/submissions.csv.zip?splitSelectMultiples=true&groupPaths=true&deletedFields=true`);
-}
-
-function durationForHumans(ms) {
-  if(ms > 1000) return oneDp((ms / 1000)) + 's';
-  else          return oneDp(         ms) + 'ms';
-}
-
-function oneDp(n) {
-  return Number(n.toFixed(1));
-}
-
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms)); // eslint-disable-line no-promise-executor-return
 }
