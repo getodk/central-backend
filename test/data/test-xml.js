4478c09a274890d6a516dbe4061a1e17a7036bd5
brontolosone <177225737+brontolosone@users.noreply.github.com>
2025-08-22 07:11:39 +0000

    test the tests: Form XML validity

---
+++ test/data/test-xml.js
@@ -0,0 +1,47 @@
+const libxmljs = require('libxmljs');
+const testXML = require('./xml');
+
+function* getStrings(thing) {
+  // traverse object `thing`; yield pairs of ([pathcomponent, ...], someString)
+  if (typeof thing === 'string') {
+    yield [[], thing];
+  } else {
+    for (const [k, v] of Object.entries(thing)) {
+      if (typeof v === 'string') {
+        yield [[k], v];
+      } else {
+        for (const [stack, str] of getStrings(v)) yield [[k].concat(stack), str];
+      }
+    }
+  }
+}
+
+function isPalatableXML(allegedXML) {
+  const xmlDoc = libxmljs.parseXml(
+    allegedXML,
+    {
+      nonet: true,
+      validateAttributes: true,
+    }
+  );
+  if (xmlDoc.errors.length) {
+    throw new Error(`XML errors:\n${xmlDoc.errors.join('\n')}`);
+  }
+  if (xmlDoc.validationErrors.length) {
+    throw new Error(`Validation errors:\n${xmlDoc.validationErrors.join('\n')}`);
+  }
+}
+
+describe('Validity of form testdata XML', () => {
+  it('should be well-formed XML', () => {
+    getStrings(testXML.forms).forEach(([stack, str]) => {
+      try {
+        isPalatableXML(str);
+      } catch (err) {
+        const myErr = new Error(`Invalid XML encountered at forms.${stack.join('.')}`);
+        myErr.cause = err;
+        throw myErr;
+      }
+    });
+  });
+});
