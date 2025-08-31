224b9c91670b8698b4789d7c0aaff38c0de4c7bc
brontolosone <177225737+brontolosone@users.noreply.github.com>
2025-08-31 15:01:29 +0000

    API for on-the-fly entity geometry extraction

--- lib/model/migrations/20250830-01-geoextracts-01.up.sql
+++ lib/model/migrations/20250830-01-geoextracts-01.up.sql
@@ -8,3 +8,9 @@ CREATE TABLE submission_field_extract_geo_cache (
 
 -- this index is used to skip extraction tombstones (no (valid) geovalue found)
 CREATE INDEX submission_field_extract_geo_cache_non_null_geovalue ON submission_field_extract_geo_cache ((geovalue IS NOT NULL));
+
+-- this index is used as a first pass when selecting extraction candidates;
+-- we want to skip any of which we can very quickly establish that they
+-- don't contain any extractable geodata.
+CREATE INDEX entity_defs_has_geometry ON entity_defs (id)
+    WHERE data ? 'geometry' AND data ->> 'geometry' ~ '\d';
