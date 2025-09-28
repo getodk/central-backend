035e590898d6d6f324d30973913bc31d6d5e0631
brontolosone <177225737+brontolosone@users.noreply.github.com>
2025-09-28 09:25:10 +0000

    backfill submission geocache, fixes getodk/central#1373

---
+++ lib/model/migrations/20250928-01-backfill-submission-geocache-createfunction-01.down.sql
@@ -0,0 +1,4 @@
+
+--- drop: cache_all_submission_geo(only_default_path boolean, batchsize integer) ---
+DROP FUNCTION IF EXISTS "public"."cache_all_submission_geo"(only_default_path boolean, batchsize integer) CASCADE;
+
