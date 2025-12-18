1b27a5fc28be999abcefc9eae7b0c92fdc35b1ab
brontolosone <177225737+brontolosone@users.noreply.github.com>
2025-12-18 14:55:54 +0000

    Basic test for row event stamping

---
+++ test/integration/other/db-eventstamping.js
@@ -0,0 +1,59 @@
+const { testService } = require('../setup');
+const { sql } = require('slonik');
+const { instances } = require('../../data/xml');
+
+
+describe('DB: Event stamping', () => {
+
+  it('Event stamps are applied upon submission insertion and modification', testService(async (service, { db }) => {
+    // As we are testing for the effect of a commit, we have to... commit. And then clean up.
+    try {
+      const asAlice = await service.login('alice');
+
+      await asAlice.post('/v1/projects/1/forms/simple/submissions')
+        .set('Content-Type', 'application/xml')
+        .send(instances.simple.one)
+        .expect(200);
+
+      await asAlice.post('/v1/projects/1/forms/simple/submissions')
+        .set('Content-Type', 'application/xml')
+        .send(instances.simple.two)
+        .expect(200);
+
+      await db.query(sql`commit;`);
+      // .one and .two were created in the same transaction, so they should receive the same event stamp
+      (await db.oneFirst(sql`select distinct(event) from submissions`)).should.equal(1);
+
+      await asAlice.post('/v1/projects/1/forms/simple/submissions')
+        .set('Content-Type', 'application/xml')
+        .send(instances.simple.three)
+        .expect(200);
+
+      await db.query(sql`commit;`);
+      (await db.oneFirst(sql`select event from submissions where "instanceId" = 'three'`)).should.equal(2);
+
+      await asAlice.put('/v1/projects/1/forms/simple/submissions/one')
+        .set('Content-Type', 'application/xml')
+        .send('<data id="simple"><meta><instanceID>oneA</instanceID><deprecatedID>one</deprecatedID></meta><whatevs>bla</whatevs></data>')
+        .expect(200);
+
+      await db.query(sql`commit;`);
+      (await db.oneFirst(sql`select event from submissions where "instanceId" = 'one'`)).should.equal(3);
+
+      await asAlice.patch('/v1/projects/1/forms/simple/submissions/one')
+        .set('Content-Type', 'application/json')
+        .send({ reviewState: 'approved' })
+        .expect(200);
+
+      await db.query(sql`commit;`);
+      (await db.oneFirst(sql`select event from submissions where "instanceId" = 'one'`)).should.equal(4);
+
+    } finally {
+      await db.query(sql`truncate table submissions cascade`);
+      await db.query(sql`truncate table audits cascade`);
+      await db.query(sql`commit;`);
+    }
+
+  }));
+
+});
