d6336812536f3faadfc5ad344e79c03e79120683
Kathleen Tuite <ktuite@getodk.org>
2026-02-18 15:36:44 -0800

    Change capitalization

--- test/integration/task/purge.js
+++ test/integration/task/purge.js
@@ -325,7 +325,7 @@ describe('task: purge deleted resources (forms, submissions and entities)', () =
         .then((form) => Forms.del(form.get())
           .then(() => purgeTask())
           .then((message) => {
-            message.should.equal('Forms purged: 0, Submissions purged: 0, Datasets Purged: 0, Entities purged: 0');
+            message.should.equal('Forms purged: 0, Submissions purged: 0, Datasets purged: 0, Entities purged: 0');
           }))));
 
     it('should purge both forms and submissions when neither mode is specified (forced)', testTask(({ Forms }) =>
@@ -333,7 +333,7 @@ describe('task: purge deleted resources (forms, submissions and entities)', () =
         .then((form) => Forms.del(form.get())
           .then(() => purgeTask({ force: true }))
           .then((message) => {
-            message.should.equal('Forms purged: 1, Submissions purged: 0, Datasets Purged: 0, Entities purged: 0');
+            message.should.equal('Forms purged: 1, Submissions purged: 0, Datasets purged: 0, Entities purged: 0');
           }))));
 
     it('should accept other mode and treat as "all"', testTask(({ Forms }) =>
@@ -341,7 +341,7 @@ describe('task: purge deleted resources (forms, submissions and entities)', () =
         .then((form) => Forms.del(form.get())
           .then(() => purgeTask({ force: true, mode: 'something_else' }))
           .then((message) => {
-            message.should.equal('Forms purged: 1, Submissions purged: 0, Datasets Purged: 0, Entities purged: 0');
+            message.should.equal('Forms purged: 1, Submissions purged: 0, Datasets purged: 0, Entities purged: 0');
           }))));
   });
 
