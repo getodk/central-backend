3a917c5ad05bf0859ecc1e08c47e7f1ba771f03a
alxndrsn <alxndrsn>
2026-04-13 12:56:30 +0000

    wip

---
+++ test/integration/external/sentry.js
@@ -0,0 +1,104 @@
+const assert = require('node:assert/strict');
+const { execSync, spawn } = require('node:child_process');
+
+const express = require('express');
+const { v4:uuid } = require('uuid');
+
+describe.only('sentry', () => {
+  describe('task integration', () => {
+    let server, port;
+
+    beforeEach(() => new Promise((resolve, reject) => {
+      const log = (...args) => console.log('[mock-sentry]', ...args);
+
+      const events = [];
+
+      const app = express();
+      //app.use(express.json());
+      app.use(express.text({ type:() => true, limit:'5mb' }));
+      app.use((req, res, next) => {
+        const { method, path, headers, query, params, body } = req;
+        log('received request:', method, path, { headers, query, params, body });
+        next();
+      });
+      app.get('/event-log', (req, res) => {
+        log('/event-log returning:', events);
+        res.send(events);
+      });
+      app.all('/*', (req, res) => {
+        const parts = req.body.split('\n').filter(it => it).map(it => JSON.parse(it));
+        if(parts.length !== 3) throw new Error(`unrecognised part count: ${parts.length}`);
+
+        const [ metadata, typeContainer, data ] = parts;
+
+        const { type } = typeContainer;
+        if(!type) throw new Error('No type property found in typeContainer.');
+
+        if(type === 'event') events.push({ metadata, data });
+
+        res.send({ id:uuid().replace(/-/g, '') });
+      });
+
+      server = app.listen(0, () => {
+        port = server.address().port;
+        resolve();
+      });
+
+      server.on('error', reject);
+    }));
+    afterEach(() => {
+      server?.close();
+    });
+
+    it('should include odk-task tag in error event', async function() {
+      //this.timeout(20_000);
+
+      // given
+      const env = {
+        ...process.env, // ensure NodeJS is available in child process
+        NODE_CONFIG: JSON.stringify({
+          default: {
+            external: {
+              sentry: {
+                key: 'deadbeefcafe',
+                orgSubdomain: 'o123',
+                project: 123,
+                tunnel: `http://localhost:${port}/dsn`,
+              },
+            },
+          },
+        }),
+      };
+
+      // when
+      console.log('XXX calling script...');
+      //assert.throws(() => execSync('node lib/bin/test-sentry-logging test error', { env, stdio:'inherit' }));
+      //execSync('node lib/bin/test-sentry-logging test error', { env, stdio:'inherit' });
+      const child = spawn('node', ['lib/bin/test-sentry-logging', 'test error'], { env, stdio:'inherit' });
+      await new Promise(resolve => child.on('close', resolve));
+      console.log('XXX script returned');
+
+      // and
+      //await sleep(10000);
+      console.log('sleeping...');
+      await sleep(100);
+      console.log('woke up');
+
+      // then
+      const loggedEvents = await getLoggedEvents();
+      assert.equal(loggedEvents.length, 1);
+      const [ e ] = loggedEvents;
+      assert.deepEqual(e.data.tags, { 'odk-task':'test-sentry-logging' });
+    });
+
+    async function getLoggedEvents() { // eslint-disable-line no-use-before-define
+      const res = await fetch(`http://localhost:${port}/event-log`);
+      assert.equal(res.status, 200);
+      return await res.json();
+    }
+  });
+
+  function sleep(ms) { // eslint-disable-line no-use-before-define
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+});
