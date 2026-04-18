getodk/central-backend

commit eabd14c443ac7818c83546b89075021839339303

new: add endpoint to create Enketo form previews…
as required by opendatakit/central-frontend#272.

for this to do anything useful, Central's configuration must include:

    "enketo": {
      "url": "https://some/enketo/service",
      "apiKey": "corresponding api key"
    }

lib/util/problem.js
@@ -154,7 +154,16 @@ const problems = {
     timeout: problem(502.1, () => 'The task took too long to run.'),
 
     // returned if an xlsform service is configured but it cannot be contacted.
-    xlsformNotAvailable: problem(502.2, () => 'The XLSForm conversion service could not be contacted.')
+    xlsformNotAvailable: problem(502.2, () => 'The XLSForm conversion service could not be contacted.'),
+
+    // returned in case no Enketo service is configured.
+    enketoNotConfigured: problem(501.4, () => 'This ODK Central has not been configured to use Enketo. Please contact your server administrator.'),
+
+    // returned if Enketo is configured but it cannot be contacted.
+    enketoNotAvailable: problem(502.3, () => 'Enketo could not be contacted.'),
+
+    // returned if Enketo returns an unexpected response.
+    enketoUnexpectedResponse: problem(500.4, () => 'The Enketo service returned an unexpected response.')
   }
 };
 

lib/bin/run-server.js
@@ -35,12 +35,15 @@ const Sentry = require('../util/sentry').init(config.get('default.external.sentr
 const xlsform = require('../util/xlsform').init(config.get('default.xlsform'));
 const crypto = require('../util/crypto');
 
+// get an Enketo client
+const enketo = require('../util/enketo').init(config.get('default.enketo'));
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // START HTTP SERVICE
 
 // initialize our container, then generate an http service out of it.
-const container = require('../model/package').withDefaults({ db, mail, env, google, Sentry, crypto, xlsform });
+const container = require('../model/package').withDefaults({ db, mail, env, google, Sentry, crypto, xlsform, enketo });
 const service = require('../http/service')(container);
 
 // insert the graceful exit middleware.

lib/model/instance/session.js
@@ -25,9 +25,13 @@ module.exports = Instance('sessions', {
   create() { return simply.create('sessions', this); }
   delete() { return simply.delete('sessions', this, 'token'); }
 
-  static fromActor(actor) {
-    const expiresAt = new Date();
-    expiresAt.setDate(expiresAt.getDate() + 1);
+  // 24 hours is used unless expiresAt is explicitly given.
+  static fromActor(actor, explicitExpiresAt) {
+    const expiresAt = Option.of(explicitExpiresAt).orElseGet(() => {
+      const date = new Date();
+      date.setDate(date.getDate() + 1);
+      return date;
+    });
     return new Session({ actorId: actor.id, token: generateToken(), csrf: generateToken(), expiresAt });
   }
 

lib/resources/forms.js
@@ -258,6 +258,25 @@ module.exports = (service, endpoint) => {
         ]))
         .then(([ updated ]) => updated))));
 
+  // create an Enketo preview of the form by creating a 15-minute session on behalf
+  // of the authenticated actor, POSTing to Enketo's `/api/v2/survey/preview`, and
+  // returning the response
+  service.post('/projects/:projectId/forms/:id/preview', endpoint(({ Form, Session, enketo, env }, { params, auth }, request, response) =>
+    Form.getByProjectAndXmlFormId(params.projectId, params.id)
+      .then(getOrNotFound)
+      .then((form) => auth.canOrReject('form.read', form))
+      .then((form) => {
+        const expiresAt = new Date();
+        expiresAt.setMinutes(expiresAt.getMinutes() + 15);
+
+        // TODO: can't happen yet but what if there is no actor?
+        return Session.fromActor(auth.actor().get(), expiresAt).create()
+          .then((session) => {
+            const openRosaUrl = `${env.domain}/v1/projects/${params.projectId}`;
+            return enketo.preview(openRosaUrl, form.xmlFormId, session.token, response);
+          });
+      })));
+
   service.delete('/projects/:projectId/forms/:id', endpoint(({ Form, Audit }, { auth, params }) =>
     Form.getByProjectAndXmlFormId(params.projectId, params.id)
       .then(getOrNotFound)

lib/util/enketo.js
@@ -1,0 +1,78 @@
+// Copyright 2019 ODK Central Developers
+// See the NOTICE file at the top-level directory of this distribution and at
+// https://github.com/opendatakit/central-backend/blob/master/NOTICE.
+// This file is part of ODK Central. It is subject to the license terms in
+// the LICENSE file found in the top-level directory of this distribution and at
+// https://www.apache.org/licenses/LICENSE-2.0. No part of ODK Central,
+// including this file, may be copied, modified, propagated, or distributed
+// except according to the terms contained in the LICENSE file.
+
+// here we support previewing forms (and hopefully other things in the future)
+// by providing a very thin wrapper around node http requests to Enketo. given
+// an OpenRosa endpoint to access a form's xml and its media, Enketo should
+// return a preview url, which we then pass untouched to the client.
+
+const http = require('http');
+const https = require('https');
+const path = require('path');
+const querystring = require('querystring');
+const { isBlank } = require('./util');
+const Problem = require('./problem');
+
+const mock = {
+  preview: () => Promise.reject(Problem.internal.enketoNotConfigured())
+};
+
+const enketo = (hostname, pathname, port, protocol, apiKey) => ({
+  preview: (openRosaUrl, xmlFormId, token, response) => new Promise((resolve, reject) => {
+    const previewPath = path.posix.join(pathname, 'api/v2/survey/preview');
+    const auth = `${apiKey}:`;
+    const postData = querystring.stringify({ server_url: openRosaUrl, form_id: xmlFormId });
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(postData),
+      Cookie: `__Host-session=${token}`
+    };
+
+    const request = protocol === 'https:' ? https.request : http.request;
+    const req = request({ hostname, port, headers, method: 'POST', path: previewPath, auth }, (res) => {
+      const resData = [];
+      res.on('data', (d) => { resData.push(d); });
+      res.on('error', reject);
+      res.on('end', () => {
+        let body;
+        try { body = JSON.parse(Buffer.concat(resData)); } catch (ex) {
+          reject(Problem.internal.enketoUnexpectedResponse('invalid JSON'));
+        }
+        if (res.statusCode === 200 || res.statusCode === 201) {
+          response.statusCode = res.statusCode;
+          resolve(body);
+        } else reject(Problem.internal.enketoUnexpectedResponse('wrong status code'));
+      });
+    });
+
+    req.on('error', (error) => { reject(Problem.internal.enketoNotAvailable({ error })); });
+
+    req.write(postData);
+    req.end();
+  })
+});
+
+// sorts through config and returns an object containing stubs or real functions for Enketo integration.
+// (okay, right now it's just one function)
+const init = (config) => {
+  if (config == null) return mock;
+  if (isBlank(config.url) || isBlank(config.apiKey)) return mock;
+  let parsedUrl;
+  try {
+    parsedUrl = new URL(config.url);
+  } catch (ex) {
+    if (ex instanceof TypeError) return mock; // configuration has an invalid Enketo URL
+    else throw ex;
+  }
+  const { hostname, pathname, port, protocol } = parsedUrl;
+  return enketo(hostname, pathname, port, protocol, config.apiKey);
+};
+
+module.exports = { init };
+

test/integration/api/forms.js
@@ -731,6 +731,31 @@ describe('api: /projects/:id/forms', () => {
               })))));
     });
 
+    describe('/preview POST', () => {
+      it('should reject unless the user can read', testService((service) =>
+        service.login('chelsea', (asChelsea) =>
+          asChelsea.post('/v1/projects/1/forms/simple/preview').expect(403))));
+
+      it('should return json with preview url and code', testService((service) =>
+        service.login('alice', (asAlice) =>
+          asAlice.post('/v1/projects/1/forms/simple/preview')
+            .expect(201)
+            .then(({ body }) =>
+              body.should.eql({ preview_url: 'http://enke.to/preview/::abcdefgh', code: 201 })
+            ))));
+
+      it('should fail if Enketo returns an unexpected response', testService((service) => {
+        global.enketoPreviewTest = 'error'; // set up the mock service to fail.
+        return service.login('alice', (asAlice) =>
+          asAlice.post('/v1/projects/1/forms/simple/preview')
+            .expect(500)
+            .then(({ body }) => {
+              body.code.should.equal(500.4);
+              body.message.should.equal('The Enketo service returned an unexpected response.');
+            }))
+      }));
+    });
+
     ////////////////////////////////////////
     // SUBRESOURCES
 

test/integration/setup.js
@@ -42,6 +42,9 @@ const crypto = (process.env.BCRYPT === 'no')
   ? require('../util/crypto-mock')
   : require(appRoot + '/lib/util/crypto');
 
+// set up our enketo mock.
+const enketo = require(appRoot + '/test/util/enketo');
+
 // application things.
 const injector = require(appRoot + '/lib/model/package');
 const service = require(appRoot + '/lib/http/service');
@@ -97,7 +100,7 @@ const augment = (service) => {
 ////////////////////////////////////////////////////////////////////////////////
 // FINAL TEST WRAPPERS
 
-const baseContainer = injector.withDefaults({ db, mail, env, xlsform, google, crypto, Sentry });
+const baseContainer = injector.withDefaults({ db, mail, env, xlsform, google, crypto, enketo, Sentry });
 
 // called to get a service context per request. we do some work to hijack the
 // transaction system so that each test runs in a single transaction that then

test/unit/util/enketo.js
@@ -1,0 +1,64 @@
+const appRoot = require('app-root-path');
+const nock = require('nock');
+const querystring = require('querystring');
+const should = require('should');
+const enketo_ = require(appRoot + '/lib/util/enketo');
+const Problem = require(appRoot + '/lib/util/problem');
+
+describe('util/enketo', () => {
+  const enketoConfig = {
+    url: 'http://enketoHost:1234/enketoPath',
+    apiKey: 'enketoApiKey'
+  };
+  const enketo = enketo_.init(enketoConfig);
+  const enketoNock = nock('http://enketoHost:1234');
+  const openRosaUrl = 'http://openRosaHost:5678/somePath';
+  const xmlFormId = 'wellPumps';
+
+  describe('preview', () => {
+    it('should send a properly constructed request to Enketo', () => {
+      enketoNock
+        .post('/enketoPath/api/v2/survey/preview')
+        .reply(201, function(uri, requestBody) {
+          const base64Auth = Buffer.from('enketoApiKey:').toString('base64');
+          const expectedQueryString = querystring.stringify({ server_url: openRosaUrl, form_id: xmlFormId });
+          this.req.headers.authorization.should.equal(`Basic ${base64Auth}`);
+          this.req.headers.cookie.should.equal('__Host-session=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+          requestBody.should.equal(expectedQueryString);
+          return JSON.stringify({ preview_url: 'http://enke.to/preview/::stuvwxyz', code: 201 });
+        });
+      const response = {};
+      console.log(openRosaUrl);
+      return enketo.preview(openRosaUrl, xmlFormId, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', response);
+    });
+
+    it('should pass through the intact Enketo response', () => {
+      enketoNock
+        .post('/enketoPath/api/v2/survey/preview')
+        .reply(201, { preview_url: 'http://enke.to/preview/::stuvwxyz', code: 201 });
+      const response = {};
+      return enketo.preview(openRosaUrl, xmlFormId, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', response)
+        .then((result) => result.should.eql({ preview_url: 'http://enke.to/preview/::stuvwxyz', code: 201 }))
+        .then(() => response.statusCode.should.equal(201));
+    });
+
+    it('should throw a Problem if the Enketo response is not valid json', () => {
+      enketoNock
+        .post('/enketoPath/api/v2/survey/preview')
+        .reply(201, 'no json for you!');
+      const response = {};
+      return enketo.preview(openRosaUrl, xmlFormId, null, response)
+        .should.be.rejectedWith(Problem.internal.enketoUnexpectedResponse('invalid JSON'));
+    });
+
+    it('should throw a Problem if the Enketo response code is unexpected', () => {
+      enketoNock
+        .post('/enketoPath/api/v2/survey/preview')
+        .reply(204, {});
+      const response = {};
+      return enketo.preview(openRosaUrl, xmlFormId, null, response)
+        .should.be.rejectedWith(Problem.internal.enketoUnexpectedResponse('wrong status code'));
+    });
+  });
+});
+

commit c5b10441071ef9382984de460df4346ea902845e

util/problem: reject problems with duplicate codes (#1803)
eabd14c443ac7818c83546b89075021839339303 introduced new problem codes, but broke conventional ordering.

Due to lack of tests, this allowed later introduction of problems redefining the same error numbers.

This commit:

* changes construction of `problem.internal.invalidHeader` to be consistent with all other problems
* re-orders all problems to be sorted numerically by code
* renumbers s3 problem codes, because:
  * the initial duplicate was `problem.internal.s3accessDenied` (formerly `500.4`, clashing with `problem.internal.enketoUnexpectedResponse`)
  * the next duplicate was `problem.internal.analyticsUnexpectedResponse` (formerly `500.5`, clashing with `problem.internal.s3upstreamError`), but it seems pragmatic to keep all the renumbering in the same functional area
* adds tests to protect against mis-ordering & repeat numbering

lib/model/query/audits.js
@@ -23,7 +23,7 @@ const log = (actor, action, actee, details) => ({ run, context }) => {
   const acteeId = Option.of(actee).map((x) => x.acteeId).orNull();
   const processed = Audit.actionableEvents.includes(action) ? null : sql`clock_timestamp()`;
 
-  const notes = Option.of(context?.headers[xActionNotes]).map(val => urlDecode(val).orThrow(Problem.user.invalidHeader(xActionNotes))).orNull();
+  const notes = Option.of(context?.headers[xActionNotes]).map(val => urlDecode(val).orThrow(Problem.user.invalidHeader({ field: xActionNotes }))).orNull();
 
   return run(sql`
 insert into audits ("actorId", action, "acteeId", details, notes, "loggedAt", processed, failures)

lib/util/problem.js
@@ -14,7 +14,6 @@
 // A list of our Problems with their codes and messages can be found below.
 
 const { floor } = Math;
-const { ifElse, is } = require('ramda');
 const { printPairs } = require('./util');
 
 class Problem extends Error {
@@ -66,11 +65,10 @@ const problems = {
     // 400.5 (uniquenessViolation), was used in the past, but has been deprecated
 
     // { header: "the problematic header", value: "the supplied (problematic) value" }
-    invalidHeader: ifElse(
-      is(String),
-      field => new Problem(400.6, `An expected header field (${field}) did not match the expected format.`, { field }),
-      problem(400.6, ({ field, value }) => `An expected header field (${field}) did not match the expected format (got: ${(value == null) ? '[nothing]' : value}).`),
-    ),
+    invalidHeader: problem(400.6, ({ field, value }) => {
+      if (typeof value === 'undefined') return `An expected header field (${field}) did not match the expected format.`;
+      else return `An expected header field (${field}) did not match the expected format (got: ${(value == null) ? '[nothing]' : value}).`;
+    }),
 
     // { field: "the name of the missing field" }
     missingMultipartField: problem(400.7, ({ field }) => `Required multipart POST field ${field} missing.`),
@@ -219,12 +217,12 @@ const problems = {
     // tried to create a form while encryption was being enabled.
     encryptionActivating: problem(409.5, () => 'Could not create form, because the project was being prepared for managed encryption. Please try again in a moment.'),
 
-    // tried to delete a draft w no published version.
-    noPublishedVersion: problem(409.7, () => 'Could not delete draft, because the form has not been published. Either delete the whole form, or replace the draft.'),
-
     // special version of uniquenessViolation to be more informative about the publish-version
     // conflict case.
     versionUniquenessViolation: problem(409.6, ({ xmlFormId, version }) => `You tried to publish the form '${xmlFormId}' with version '${version}', but a published form has already existed in this project with those identifiers.`),
+
+    // tried to delete a draft w no published version.
+    noPublishedVersion: problem(409.7, () => 'Could not delete draft, because the form has not been published. Either delete the whole form, or replace the draft.'),
 
     // tried to reuse an instanceId
     instanceIdConflict: problem(409.8, () => 'You tried to update a submission, but the instanceID you provided already exists on this server. Please try again with a unique instanceID.'),
@@ -266,9 +264,17 @@ const problems = {
     // only called for development, theoretically.
     emptyResponse: problem(500.3, () => 'The resource returned no data. This is likely a developer problem.'),
 
-    s3accessDenied: problem(500.4, () => `The S3 account details or permissions are incorrect.`),
+    // returned if Enketo returns an unexpected response.
+    enketoUnexpectedResponse: problem(500.4, () => 'The Enketo service returned an unexpected response.'),
 
-    s3upstreamError: problem(500.5, ({ amzRequestId, operation }) => `The upstream S3 server had an internal problem performing '${operation}'. Amazon request ID: '${amzRequestId}'.`),
+    // returned if ODK Analytics returns an unexpected response.
+    analyticsUnexpectedResponse: problem(500.5, () => 'The Analytics service returned an unexpected response.'),
+
+    fieldTypesNotDefined: problem(500.6, (frame) => `fieldTypes are not defined on the ${frame} Frame, please define them to use insertMany.`),
+
+    s3accessDenied: problem(500.7, () => `The S3 account details or permissions are incorrect.`),
+
+    s3upstreamError: problem(500.9, ({ amzRequestId, operation }) => `The upstream S3 server had an internal problem performing '${operation}'. Amazon request ID: '${amzRequestId}'.`),
 
     // used to indicate missing odata functionality.
     notImplemented: problem(501.1, ({ feature }) => `The requested feature ${feature} is not supported by this server.`),
@@ -302,14 +308,6 @@ const problems = {
 
     // returned if Enketo is configured but it cannot be contacted.
     enketoNotAvailable: problem(502.3, () => 'Enketo could not be contacted.'),
-
-    // returned if Enketo returns an unexpected response.
-    enketoUnexpectedResponse: problem(500.4, () => 'The Enketo service returned an unexpected response.'),
-
-    // returned if ODK Analytics returns an unexpected response.
-    analyticsUnexpectedResponse: problem(500.5, () => 'The Analytics service returned an unexpected response.'),
-
-    fieldTypesNotDefined: problem(500.6, (frame) => `fieldTypes are not defined on the ${frame} Frame, please define them to use insertMany.`)
   }
 };
 

test/unit/external/s3.js
@@ -81,7 +81,7 @@ describe('external/s3', () => {
         {
           name: 'Error',
           message: 'The S3 account details or permissions are incorrect.',
-          problemCode: 500.4,
+          problemCode: 500.7,
           problemDetails: {
             amzRequestId,
             operation: 'removeObjects',
@@ -101,7 +101,7 @@ describe('external/s3', () => {
         {
           name: 'Error',
           message: `The upstream S3 server had an internal problem performing 'removeObjects'. Amazon request ID: 'tx000000000000000000000-0000000000-000000001-xxxxx'.`,
-          problemCode: 500.5,
+          problemCode: 500.9,
           problemDetails: {
             amzRequestId,
             operation: 'removeObjects',
@@ -124,7 +124,7 @@ describe('external/s3', () => {
         {
           name: 'Error',
           message: 'The S3 account details or permissions are incorrect.',
-          problemCode: 500.4,
+          problemCode: 500.7,
           problemDetails: {
             amzRequestId,
             operation: 'getObject',
@@ -144,7 +144,7 @@ describe('external/s3', () => {
         {
           name: 'Error',
           message: `The upstream S3 server had an internal problem performing 'getObject'. Amazon request ID: 'tx000000000000000000000-0000000000-000000001-xxxxx'.`,
-          problemCode: 500.5,
+          problemCode: 500.9,
           problemDetails: {
             amzRequestId,
             operation: 'getObject',
@@ -172,7 +172,7 @@ describe('external/s3', () => {
         {
           name: 'Error',
           message: 'The S3 account details or permissions are incorrect.',
-          problemCode: 500.4,
+          problemCode: 500.7,
           problemDetails: {
             amzRequestId,
             operation: 'putObject',
@@ -192,7 +192,7 @@ describe('external/s3', () => {
         {
           name: 'Error',
           message: `The upstream S3 server had an internal problem performing 'putObject'. Amazon request ID: 'tx000000000000000000000-0000000000-000000001-xxxxx'.`,
-          problemCode: 500.5,
+          problemCode: 500.9,
           problemDetails: {
             amzRequestId,
             operation: 'putObject',

test/unit/util/problem.js
@@ -1,0 +1,65 @@
+const appRoot = require('app-root-path');
+const Problem = require(appRoot + '/lib/util/problem');
+
+describe('Problem', () => {
+  const expectedRanges = {
+    user: 400,
+    internal: 500,
+  };
+
+  for (const [ type, problemMap ] of Object.entries(Problem)) {
+    describe(`type: ${type}`, () => {
+      const expectedRange = expectedRanges[type];
+
+      it('should define an expected code range', () => {
+        Number.isFinite(expectedRange).should.be.true();
+        (Math.floor(expectedRange / 100) * 100).should.eql(expectedRange);
+      });
+
+      it('should declare all Problems in numeric order', () => {
+        const declaredCodes = Object.values(problemMap).map(({ code }) => code);
+
+        const sortedCodes = [ ...declaredCodes ].sort((a, b) => {
+          const [intA, frcA] = a.toString().split('.');
+          const [intB, frcB] = b.toString().split('.');
+
+          if (intA !== intB) return intA - intB;
+          if (frcA !== frcB) return frcA - frcB;
+
+          return 0;
+        });
+
+        declaredCodes.should.eql(sortedCodes);
+      });
+
+      it('should not have duplicate Problem IDs', () => {
+        const duplicates = Object.entries(
+          Object
+            .entries(problemMap)
+            .reduce(
+              (acc, [ name, { code } ]) => {
+                if (!acc[code]) acc[code] = [];
+                acc[code].push(name);
+                return acc;
+              },
+              {},
+            ),
+        ).filter(([ , names ]) => names.length > 1);
+
+        duplicates.should.eql([], 'One or more numeric codes is shared by multiple Problems.');
+      });
+
+      for (const [ name, { code } ] of Object.entries(problemMap)) {
+        describe(`problem: ${name}`, () => {
+          it('should define a numeric code', () => {
+            Number.isFinite(code).should.be.true('Code is not a valid number.');
+          });
+
+          it(`should be within ${expectedRange.toString().replace(/0/g, 'x')}`, () => {
+            (Math.floor(code / 100) * 100).should.eql(expectedRange);
+          });
+        });
+      }
+    });
+  }
+});