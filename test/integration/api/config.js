98f610fb24621b0fe1776700f8bdcb6a44fbd4aa
alxndrsn <alxndrsn>
2026-05-07 10:07:36 +0000

    Update cache policy handling to be more descriptive

--- test/integration/api/config.js
+++ test/integration/api/config.js
@@ -233,7 +233,7 @@ describe('api: /config', () => {
                       .then(assertStandardResponse)
                       .then(({ headers }) => {
                         headers['cache-control'].should.eql('no-cache');
-                        should(headers['vary']).be.equalOneOf(undefined, 'Accept-Encoding, Origin'); // eslint-disable-line dot-notation
+                        should(headers['vary']).be.undefined(); // eslint-disable-line dot-notation
                       });
                   }));
                 });
@@ -244,7 +244,7 @@ describe('api: /config', () => {
                     .then(assertStandardResponse)
                     .then(({ headers }) => {
                       headers['cache-control'].should.eql('max-age=31536000');
-                      should(headers['vary']).be.equalOneOf(undefined, 'Accept-Encoding, Origin'); // eslint-disable-line dot-notation
+                      should(headers['vary']).be.undefined(); // eslint-disable-line dot-notation
                     });
                 }));
 
