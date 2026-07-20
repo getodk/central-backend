781f73fb39670c0cc05e0e1ab030ac7baf984a14
alxndrsn <alxndrsn>
2026-07-20 06:26:57 +0000

    add tests

--- test/integration/other/database.js
+++ test/integration/other/database.js
@@ -3,22 +3,24 @@ const { sql } = require('slonik');
 const { testContainer } = require('../setup');
 
 describe('database', () => {
-  it.only('should allow reading postgres stats', testContainer(async ({ all }) => {
-    const stats = await all(sql`
-      SELECT query
-           , calls
-           , round(total_exec_time::numeric, 2) AS total_ms
-           , round(mean_exec_time::numeric, 2) AS mean_ms
-           , rows
-        FROM pg_stat_statements
-        ORDER BY total_exec_time DESC
-    `);
+  describe('pg_stat_statements', () => {
+    it.only('should allow reading postgres stats', testContainer(async ({ all }) => {
+      const stats = await all(sql`
+        SELECT query
+             , calls
+             , round(total_exec_time::numeric, 2) AS total_ms
+             , round(mean_exec_time::numeric, 2) AS mean_ms
+             , rows
+          FROM pg_stat_statements
+          ORDER BY total_exec_time DESC
+      `);
 
-    stats.should.have.property('rows');
-  }));
+      stats.should.have.property('rows');
+    }));
 
-  it.only('should allow clearing postgres stats', testContainer(async ({ run }) => {
-    const cleared = await run(sql`SELECT pg_stat_statements_reset()`);
-    cleared.should.be.true();
-  }));
+    it.only('should allow clearing postgres stats', testContainer(async ({ run }) => {
+      const cleared = await run(sql`SELECT pg_stat_statements_reset()`);
+      cleared.should.be.true();
+    }));
+  });
 });
