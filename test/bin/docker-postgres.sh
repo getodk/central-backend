2ed667b181e5484338f4aca01fa5d631ff3b735e
alxndrsn <alxndrsn>
2026-09-15 07:45:00 +0000

    remove lucky sleep

--- test/bin/docker-postgres.sh
+++ test/bin/docker-postgres.sh
@@ -112,10 +112,6 @@ wait_for_postgres() {
     retries=$((retries-1))
   done
 
-  # Extra sleep for luck ¯\_(ツ)_/¯ FIXME check if this can be removed before merge
-  printf >&2 .
-  sleep 1
-
   printf >&2 'OK.\n'
 }
 wait_for_postgres
