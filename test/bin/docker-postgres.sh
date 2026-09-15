82f606660c346c9aa13b20c89e729c42d5cb7fff
alxndrsn <alxndrsn>
2026-09-15 11:34:28 +0000

    whitespace

--- test/bin/docker-postgres.sh
+++ test/bin/docker-postgres.sh
@@ -121,6 +121,7 @@ node lib/bin/create-docker-databases.js ${CI:+--log}
 if [[ "$enableSsl" = true ]]; then
   log "Applying SSL config..."
   docker exec "$imageName" bash -c 'sed -i "s/^host\b/hostssl/" "$PGDATA/pg_hba.conf"'
+
   log "SSL config applied; reloading postgres config..."
   docker exec "$imageName" psql -U postgres -c 'SELECT pg_reload_conf()' >/dev/null
   wait_for_postgres --require-ssl
