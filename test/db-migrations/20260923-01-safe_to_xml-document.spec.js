13ebb9193a30d368e27e80405130f461a4251fd3
Matthew White <matthew-white@users.noreply.github.com>
2026-09-24 02:21:24 -0400

    Simplify temporary test definition of safe_to_xml()

--- test/db-migrations/20260923-01-safe_to_xml-document.spec.js
+++ test/db-migrations/20260923-01-safe_to_xml-document.spec.js
@@ -7,33 +7,23 @@ describeMigration('20260923-01-safe_to_xml-document', ({ runMigrationBeingTested
   before(async () => {
     defBeforeModification = await getFunctionDef('safe_to_xml');
 
-    // This is the old, unpatched definition of safe_to_xml() before we modified
-    // it as part of getodk/central#2261. We modify it here to mimic the case
-    // where the user ran the migration to create the function
-    // (20250927-01-geoextracts-02.up.sql) before it was fixed.
+    /* Normally, the migration would be a no-op for the test database, since the
+    migration's definition of safe_to_xml() matches the definition in
+    20250927-01-geoextracts-02.up.sql that was patched as part of
+    getodk/central#2261. However, we want to demonstrate that the migration will
+    replace the function if the user has already run the unpatched version of
+    20250927-01-geoextracts-02.up.sql. To do so, here we replace the function
+    with an incorrect definition. It should end up being replaced with the
+    correct one. */
     await db.query(sql`
 CREATE OR REPLACE FUNCTION "public"."safe_to_xml"(input text)
-RETURNS xml AS
-    $BODY$
-    DECLARE hopefully_xml xml DEFAULT NULL;
-    BEGIN
-        BEGIN
-            hopefully_xml := input::xml;
-        EXCEPTION WHEN OTHERS THEN
-            RETURN NULL;
-        END;
-    RETURN hopefully_xml;
-    END;
-    $BODY$
+RETURNS xml AS $$
+BEGIN
+    RAISE EXCEPTION 'safe_to_xml() error in testing';
+END;
+$$
 LANGUAGE plpgsql
-IMMUTABLE
-PARALLEL SAFE
 `);
-    const unpatchedDef = await getFunctionDef('safe_to_xml');
-    assert.equal(
-      unpatchedDef,
-      defBeforeModification.replace('XMLPARSE(DOCUMENT input)', 'input::xml')
-    );
 
     await runMigrationBeingTested();
   });
