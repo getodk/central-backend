64bf1ede39427893df53b1bc400732b2cea47d93
Matthew White <matthew-white@users.noreply.github.com>
2026-09-25 01:11:37 -0400

    Update comment style

--- test/db-migrations/20260923-01-safe_to_xml-document.spec.js
+++ test/db-migrations/20260923-01-safe_to_xml-document.spec.js
@@ -7,14 +7,14 @@ describeMigration('20260923-01-safe_to_xml-document', ({ runMigrationBeingTested
   before(async () => {
     defBeforeModification = await getFunctionDef('safe_to_xml');
 
-    /* Normally, the migration would be a no-op for the test database, since the
-    migration's definition of safe_to_xml() matches the definition in
-    20250927-01-geoextracts-02.up.sql that was patched as part of
-    getodk/central#2261. However, we want to demonstrate that the migration will
-    replace the function if the user has already run the unpatched version of
-    20250927-01-geoextracts-02.up.sql. To do so, here we replace the function
-    with an incorrect definition. It should end up being replaced with the
-    correct one. */
+    // Normally, the migration would be a no-op for the test database, since the
+    // migration's definition of safe_to_xml() matches the definition in
+    // 20250927-01-geoextracts-02.up.sql that was patched as part of
+    // getodk/central#2261. However, we want to demonstrate that the migration will
+    // replace the function if the user has already run the unpatched version of
+    // 20250927-01-geoextracts-02.up.sql. To do so, here we replace the function
+    // with an incorrect definition. It should end up being replaced with the
+    // correct one.
     await db.query(sql`
 CREATE OR REPLACE FUNCTION "public"."safe_to_xml"(input text)
 RETURNS xml AS $$
